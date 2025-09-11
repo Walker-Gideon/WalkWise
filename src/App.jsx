44be93cf69c420ecdbb2662910b3d1c30844d79a
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-11 19:09:51 +0000

    Added prettier config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline min-w-">Hello world!</h1>
+      <h1 class="min-w-2xs text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
