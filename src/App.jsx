849d05686a4e948f91eb077823522cde85241433
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-23 13:37:43 +0000

    Created the routes, started with the landingpage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./landingPage/LandingPage";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello walwise</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="home" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
