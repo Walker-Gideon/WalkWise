72a907283ea6266c123d63e6fd2f007be914057c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-25 22:28:38 +0000

    Set the layout for the button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,3 +1,12 @@
-export default function Button() {
-  return <button></button>;
+import useNavigateToAction from "../hook/useNavigateToAction";
+
+export default function Button({ children, to }) {
+  const navigateTo = useNavigateToAction();
+
+  // default is the navigation button
+  return (
+    <button onClick={() => navigateTo(to)} className="bg-green-500">
+      {children}
+    </button>
+  );
 }
