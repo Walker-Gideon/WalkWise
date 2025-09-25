30461b6fb507798332928a45d939f056742f5957
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-25 22:07:23 +0000

    Added the layout for the landing-page-header

--- src/landingPage/landing-page-header/LandindPageHeader.jsx
+++ src/landingPage/landing-page-header/LandindPageHeader.jsx
@@ -1,3 +1,15 @@
+import Logo from "../../ui/Logo";
+
 export default function LandindPageHeader() {
-  return <header>LandindPageHeader</header>;
+  return (
+    <header className="sticky top-0 z-50">
+      <div className="flex h-16 items-center justify-between border-b border-stone-300 bg-white/30 px-4 shadow-sm backdrop-blur-sm md:px-8 lg:px-30">
+        <Logo to="home" cursor={true} />
+        <div className="">
+          <button>Log In</button>
+          <button>Sign Up</button>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-scroll";
 
-export default function Logo({ to }) {
+export default function Logo({ to, cursor }) {
   return (
     <div>
       <Link
@@ -9,7 +9,7 @@ export default function Logo({ to }) {
         spy={true}
         duration={500}
         offset={-100}
-        className="flex cursor-pointer items-center gap-1 font-bold text-slate-500 uppercase"
+        className={`flex items-center gap-1 font-bold text-slate-500 uppercase ${cursor ? `cursor-pointer` : ``}`}
       >
         <span className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-500 text-white dark:text-slate-50">
           w
