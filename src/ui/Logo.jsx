65578c42f611f98e101b755f51404cfb898ae341
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-25 23:31:28 +0000

    Update on the styling in the logo component

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,20 +2,18 @@ import { Link } from "react-scroll";
 
 export default function Logo({ to, cursor }) {
   return (
-    <div>
-      <Link
-        to={to}
-        smooth={true}
-        spy={true}
-        duration={500}
-        offset={-100}
-        className={`flex items-center gap-1 font-bold text-slate-500 uppercase ${cursor ? `cursor-pointer` : ``}`}
-      >
-        <span className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-500 text-white dark:text-slate-50">
-          w
-        </span>
-        <span className={`hidden font-bold md:block`}>walkwise</span>
-      </Link>
-    </div>
+    <Link
+      to={to}
+      smooth={true}
+      spy={true}
+      duration={500}
+      offset={-100}
+      className={`flex items-center gap-1 font-bold text-slate-500 uppercase ${cursor ? `cursor-pointer` : ``}`}
+    >
+      <span className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-500 text-white dark:text-slate-50">
+        w
+      </span>
+      <span className={`hidden font-bold md:block`}>walkwise</span>
+    </Link>
   );
 }
