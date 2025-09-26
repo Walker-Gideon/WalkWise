040e883f9bccd9035d5b79140d41ac0bfc478e41
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-26 15:48:12 +0000

    Update the hero section with it buttons

--- src/landingPage/hero/Hero.jsx
+++ src/landingPage/hero/Hero.jsx
@@ -4,6 +4,10 @@ import Flex from "/src/ui/Flex";
 import HeaderText from "/src/ui/HeaderText";
 import Paragraph from "/src/ui/Paragraph";
 import SpanText from "/src/ui/SpanText";
+import Button from "/src/ui/Button";
+import { LuArrowDown } from "react-icons/lu";
+import { LuChevronRight } from "react-icons/lu";
+import { Link } from "react-scroll";
 
 export default function Hero() {
   return (
@@ -29,6 +33,28 @@ export default function Hero() {
             power your learning with{" "}
             <SpanText classname="font-semibold">WalkWise</SpanText>.
           </Paragraph>
+
+          <div className="">
+            <Button to="/sign-up">
+              Get Started
+              <LuChevronRight className="mt-0.5 text-sm font-bold" />
+            </Button>
+            <Link
+              to="features"
+              smooth={true}
+              spy={true}
+              duration={500}
+              offset={-100}
+            >
+              <Button
+                variant="outline"
+                classname="button border-stone-300 text-slate-700 px-4 py-[7px] font-bold flex items-center gap-2 hover:border-slate-400"
+              >
+                Explore Features
+                <LuArrowDown className="mt-0.5 text-sm font-bold text-slate-700" />
+              </Button>
+            </Link>
+          </div>
         </Box>
       </Flex>
     </Container>
