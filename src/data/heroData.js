353fb27245fd8958b5fb4227fe6ab1b2f33debb2
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-29 11:28:24 +0000

    Created the hers data file and pass date to the hero component

---
+++ src/data/heroData.js
@@ -0,0 +1,24 @@
+// /src/data/heroImages.js
+import hero1 from "/src/assets/hero1.png";
+import hero2 from "/src/assets/hero2.png";
+import hero3 from "/src/assets/hero3.png";
+
+const heroImages = [
+  {
+    text: "Create Notes",
+    url: hero1,
+    style: "-rotate-12",
+  },
+  {
+    text: "Create Flashcards",
+    url: hero2,
+    style: "mt-6",
+  },
+  {
+    text: "Schedule Flashcards",
+    url: hero3,
+    style: "rotate-12",
+  },
+];
+
+export default heroImages;
--- src/landingPage/hero/components/HeroImages.jsx
+++ src/landingPage/hero/components/HeroImages.jsx
@@ -2,27 +2,7 @@ import Container from "/src/ui/Container";
 import Paragraph from "/src/ui/Paragraph";
 import Image from "/src/ui/Image";
 import Box from "/src/ui/Box";
-import hero1 from "/src/assets/hero1.png";
-import hero2 from "/src/assets/hero2.png";
-import hero3 from "/src/assets/hero3.png";
-
-const images = [
-  {
-    text: "Create Notes",
-    url: hero1,
-    style: "-rotate-12",
-  },
-  {
-    text: "Create Flashcards",
-    url: hero2,
-    style: "mt-6",
-  },
-  {
-    text: "Schedule Flashcards",
-    url: hero3,
-    style: "rotate-12",
-  },
-];
+import heroImages from "/src/data/heroData.js";
 
 export default function HeroImages() {
   return (
@@ -31,7 +11,7 @@ export default function HeroImages() {
         adjust={true}
         classname="grid auto-cols-max grid-flow-col justify-center gap-6"
       >
-        {images.map((image, index) => (
+        {heroImages.map((image, index) => (
           <Box
             key={index}
             adjustWidth={true}
