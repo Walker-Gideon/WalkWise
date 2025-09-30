cf8f72137c3d83e18f5513adbeaeaced2a030c3b
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-30 15:53:34 +0000

    Update the information content with the image styling

--- src/landingPage/additionalInfo/components/InformationContent.jsx
+++ src/landingPage/additionalInfo/components/InformationContent.jsx
@@ -10,46 +10,46 @@ export default function InformationContent({
   points,
   url,
 }) {
-  // 
   return (
     <Container
       adjust={true}
       classname={`px-2 py-4 md:px-6 md:py-0 lg:px-15`}
     >
       <div className="flex flex-col-reverse gap-8 md:grid md:h-100 md:grid-cols-2 md:items-center lg:gap-20">
-      <div className={`${index === 1 ? `md:order-2` : ``} ${index === 3 ? `md:order-2` : ``}`}>
-        <div className="flex itemc-center space-x-4 text-sm font-semibold">
-          <span className="rounded-full p-3 bg-gradient-to-r from-slate-200 to-slate-300">{icon}</span>
-          <p>{title}</p>
+        <div className={`${index === 1 ? `md:order-2` : ``} ${index === 3 ? `md:order-2` : ``}`}>
+          <div className="flex itemc-center space-x-4 text-sm font-semibold">
+            <span className="rounded-full p-3 bg-gradient-to-r from-slate-200 to-slate-300">{icon}</span>
+            <p>{title}</p>
+          </div>
+
+          <p className="px-2 py-3 text-xl font-bold md:text-2xl">
+            {subtitle}
+          </p>
+
+          <div className="px-2">
+            {points.map((data, index) => (
+              <ul
+                key={index}
+                className="flex gap-2 py-0.5 text-sm md:text-[0.9rem]"
+              >
+                <li>
+                  <Box
+                    adjustWidth={true}
+                    classname="rounded-full p-1 bg-gradient-to-r from-slate-200 to-slate-300"
+                  >
+                    <LuCheck className="h-3 w-3 text-slate-600" />
+                  </Box>
+                </li>
+                <li className="font-medium text-slate-500">{data}</li>
+              </ul>
+            ))}
+          </div>
         </div>
 
-        <p className="px-2 py-3 text-xl font-bold md:text-2xl">
-          {subtitle}
-        </p>
-
-      <div className="px-2">
-        {points.map((data, index) => (
-          <ul
-            key={index}
-            className="flex gap-2 py-0.5 text-sm md:text-[0.9rem]"
-          >
-            <li>
-              <Box
-                adjustWidth={true}
-                classname="rounded-full p-1 bg-gradient-to-r from-slate-200 to-slate-300"
-              >
-                <LuCheck className="h-3 w-3 text-slate-600" />
-              </Box>
-            </li>
-            <li className="font-medium text-slate-500">{data}</li>
-          </ul>
-        ))}
-      </div>
-      </div>
-
-      <div>
-        <img src={url} alt="" />
-      </div>
+        <div  className={`object-cover ${index === 1 ? `md:order-1` : ``} ${index === 3 ? `md:order-1` : ``}`}>
+          <img src={url} alt="image"
+          className="w-full rounded-2xl object-scale-down shadow-md shadow-slate-600" />
+        </div>
       </div>
     </Container>
   );
