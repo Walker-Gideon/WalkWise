25c296a41a7c8a6d2af626adc51763981c50872d
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-30 17:48:26 +0000

    Created the informationOnHeader component and update the moreInfomeation component

--- src/landingPage/infoOn/MoreInformation.jsx
+++ src/landingPage/infoOn/MoreInformation.jsx
@@ -1,21 +1,11 @@
 import { LuCheck } from "react-icons/lu";
 import informationData from "/src/data/moreInformationData"
+import InformationOnHeader from "./components/InformationOnHeader"
 
 export default function MoreInformation() {
   return (
   <div className="middle:grid middle:grid-cols-3 middle:items-center middle:gap-10 bg-slate-100 p-8 md:p-14 lg:px-35 lg:py-20">
-    <header className="middle:col-span-2 mb-8 text-left font-medium">
-      <h1 className="middle:text-6xl text-4xl font-bold">Why WalkWise?</h1>
-
-      <p className="py-1 text-sm text-slate-500 md:text-base">
-        Forget last-minute cramming — learn the smarter way.
-      </p>
-
-      <p className="text-xl font-semibold md:text-2xl">
-        WalkWise isn’t just another study app. It’s your all-in-one learning
-        assistant, built for real students facing real challenges.
-      </p>
-    </header>
+    <InformationOnHeader />
 
     <div className="text-left">
       {informationData.map((data, index) => (
---
+++ src/landingPage/infoOn/components/InformationOnHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function InformationOnHeader() {
+  return (
+    <header className="middle:col-span-2 mb-8 text-left font-medium">
+      <h1 className="middle:text-6xl text-4xl font-bold">Why WalkWise?</h1>
+
+      <p className="py-1 text-sm text-slate-500 md:text-base">
+        Forget last-minute cramming — learn the smarter way.
+      </p>
+
+      <p className="text-xl font-semibold md:text-2xl">
+        WalkWise isn’t just another study app. It’s your all-in-one learning
+        assistant, built for real students facing real challenges.
+      </p>
+    </header>
+  )
+}
