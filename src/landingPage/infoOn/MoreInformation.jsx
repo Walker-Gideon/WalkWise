b9bac4a11fe427438d8f1718411e4cf275c4086b
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-02 19:13:42 +0000

    Working on the moreinformation section

--- src/landingPage/infoOn/MoreInformation.jsx
+++ src/landingPage/infoOn/MoreInformation.jsx
@@ -1,32 +1,37 @@
 import { LuCheck } from "react-icons/lu";
-import informationData from "/src/data/moreInformationData"
 import InformationOnHeader from "./components/InformationOnHeader"
+import informationData from "/src/data/moreInformationData"
+import UnorderedList from "/src/ui/UnorderedList"
+import Container from "/src/ui/Container"
+import SpanText from "/src/ui/SpanText"
+import List from "/src/ui/List"
+import Box from "/src/ui/Box"
 
 export default function MoreInformation() {
   return (
-  <div className="middle:grid middle:grid-cols-3 middle:items-center middle:gap-10 bg-slate-100 p-8 md:p-14 lg:px-35 lg:py-20">
+  <Container adjust={true} classname="middle:grid middle:grid-cols-3 middle:items-center middle:gap-10 bg-slate-100 p-8 md:p-14 lg:px-35 lg:py-20">
     <InformationOnHeader />
 
-    <div className="text-left">
+    <Box adjustWidth={true} classname="text-left">
       {informationData.map((data, index) => (
-        <div key={index} className="mb-2 flex gap-2">
-          <ul className="flex gap-2 py-0.5 text-sm md:text-[0.9rem]">
-            <li>
-              <span classname="rounded-full p-1 bg-gradient-to-r from-slate-200 to-slate-300">
+        <Box adjustWidth={true} key={index} classname="mb-2 flex gap-2">
+          <UnorderedList classname="flex gap-2 py-0.5 text-sm md:text-[0.9rem]">
+            <List>
+              <SpanText classname="rounded-full p-1 bg-gradient-to-r from-slate-200 to-slate-300">
                 <LuCheck className="h-3 w-3 text-slate-600" />
-              </span>
-            </li>
-            <li>
+              </SpanText>
+            </List>
+            <List>
               <div className="middle:text-[0.8rem] text-xs text-slate-800">
                 <p className="middle:text-[0.9rem] text-sm font-medium text-slate-900">
                   {data.header}
                 </p>
                 <p className="text-slate-500">{data.text}</p>
               </div>
-            </li>
-          </ul>
-        </div>
+            </List>
+          </UnorderedList>
+        </Box>
       ))}
-    </div>
-  </div>
+    </Box>
+  </Container>
 )};
