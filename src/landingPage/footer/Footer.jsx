a3b4447203a3d34c0e886da8eb48a9f2a54346ce
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-02 20:48:19 +0000

    Set the layout for the footer main component

--- src/landingPage/footer/Footer.jsx
+++ src/landingPage/footer/Footer.jsx
@@ -1,3 +1,19 @@
+import FooterRight from "./FooterRight"
+import Logo from "/src/ui/Logo";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="border-t border-stone-300 px-4 py-4 md:px-8 lg:px-30">
+      <div className="flex flex-col gap-4 medium:flex-row medium:items-center justify-between">
+        <div className="flex items-center gap-1 font-bold text-slate-950 uppercase">
+          <Logo logo={true} />
+          <span className={`medium:text-base text-sm font-bold text-slate-500`}>
+            walkwise
+          </span>
+        </div>
+
+        <FooterRight />
+      </div>
+    </footer>
+  );
 }
