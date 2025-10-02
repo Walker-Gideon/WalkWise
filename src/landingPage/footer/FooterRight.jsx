1e685ad6871dca82065139d502194f407e6caa5f
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-02 21:03:07 +0000

    Adjustment to the footer right component and update to the footer

--- src/landingPage/footer/FooterRight.jsx
+++ src/landingPage/footer/FooterRight.jsx
@@ -1,16 +1,20 @@
 import { LuCopyright } from "react-icons/lu";
+import Paragraph from "/src/ui/Paragraph";
+import Container from "/src/ui/Container";
+import SpanText from "/src/ui/SpanText";
 
 export default function FooterRight() {
   const date = new Date();
   const year = date.getFullYear()
+
   return (
-    <div className="w-full">
-      <p className="medium:justify-end flex gap-1 text-xs font-medium text-stone-500">
-        <span>
+    <Container adjust={true}>
+      <Paragraph type="xs" classname="medium:justify-end flex gap-1 font-medium text-stone-500">
+        <SpanText>
           <LuCopyright className="mt-0.5" />
-        </span>
-        {year} <span className="font-bold">WalkWise</span>. All rights reserved.
-      </p>
-    </div>
+        </SpanText>
+        {year} <SpanText classname="font-bold">WalkWise</SpanText>. All rights reserved.
+      </Paragraph>
+    </Container>
   )
 }
