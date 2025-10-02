b18ce7d5cb1c4e9519e01266978317b752e51ba6
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-02 20:45:36 +0000

    Set the layout for footer right component

--- src/landingPage/footer/FooterRight.jsx
+++ src/landingPage/footer/FooterRight.jsx
@@ -1,7 +1,16 @@
+import { LuCopyright } from "react-icons/lu";
+
 export default function FooterRight() {
+  const date = new Date();
+  const year = date.getFullYear()
   return (
-    <div>
-      
+    <div className="w-full">
+      <p className="medium:justify-end flex gap-1 text-xs font-medium text-stone-500">
+        <span>
+          <LuCopyright className="mt-0.5" />
+        </span>
+        {year} <span className="font-bold">WalkWise</span>. All rights reserved.
+      </p>
     </div>
   )
 }
