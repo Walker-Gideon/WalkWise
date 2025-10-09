8dbe6f94ec2141a00c4d697d275ffd4bb3bed740
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-09 21:32:24 +0000

    Adjust the auth header component styling

--- src/authentication/components/AuthHeader.jsx
+++ src/authentication/components/AuthHeader.jsx
@@ -1,15 +1,14 @@
-import Header from "../../ui/Header";
+import Header from "/src/ui/Header";
+import Logo from "/src/ui/Logo";
+import Paragraph from "/src/ui/Paragraph";
 
 export default function AuthHeader() {
   return (
-    <Header>
-      <div className="flex flex-col items-center justify-center">
-        <Logo logo={true} />
-
-        <p className="medium:text-base middle:text-lg mt-1 text-sm font-bold text-slate-500">
-          WalkWise
-        </p>
-      </div>
+    <Header classname={"flex flex-col items-center justify-center"}>
+      <Logo show={true} />
+      <Paragraph classname="medium:text-base middle:text-lg mt-1 text-sm font-bold text-slate-500">
+        WalkWise
+      </Paragraph>
     </Header>
   );
 }
