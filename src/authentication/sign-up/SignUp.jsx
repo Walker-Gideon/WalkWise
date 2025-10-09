1eb3df2def2c7d21401dfe994e1254999c20fe73
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-09 21:26:22 +0000

    Created the authentication header component

--- src/authentication/sign-up/SignUp.jsx
+++ src/authentication/sign-up/SignUp.jsx
@@ -3,8 +3,9 @@ import AuthCloseButton from "../components/AuthCloseButton";
 export default function SignUp() {
   return (
     <div>
+      <AuthCloseButton />
+
       <div className="">
-        <AuthCloseButton />
         <h1>Sign up to your account</h1>
       </div>
     </div>
