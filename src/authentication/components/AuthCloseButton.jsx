af2280e0db747f510a7da50ee7c4730cc2051e5c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-09 21:15:52 +0000

    Created the close authentication button layout

--- src/authentication/components/AuthCloseButton.jsx
+++ src/authentication/components/AuthCloseButton.jsx
@@ -1,3 +1,12 @@
+import { LuX } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
+
 export default function AuthCloseButton() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <button onClick={() => navigate("/")}>
+      <LuX className="text-2xl font-bold" />
+    </button>
+  );
 }
