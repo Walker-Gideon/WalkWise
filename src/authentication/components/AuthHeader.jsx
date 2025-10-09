646cf104c732eae307e7de72bc8fafcaefb58f3c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-09 21:28:01 +0000

    Set the auth header layout

--- src/authentication/components/AuthHeader.jsx
+++ src/authentication/components/AuthHeader.jsx
@@ -1,3 +1,15 @@
+import Header from "../../ui/Header";
+
 export default function AuthHeader() {
-  return <div></div>;
+  return (
+    <Header>
+      <div className="flex flex-col items-center justify-center">
+        <Logo logo={true} />
+
+        <p className="medium:text-base middle:text-lg mt-1 text-sm font-bold text-slate-500">
+          WalkWise
+        </p>
+      </div>
+    </Header>
+  );
 }
