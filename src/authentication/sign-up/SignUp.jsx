bc0846899d01f92cd89842cf5305f88d72c4aeef
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-09 21:57:28 +0000

    Update the sign up component

--- src/authentication/sign-up/SignUp.jsx
+++ src/authentication/sign-up/SignUp.jsx
@@ -3,6 +3,8 @@ import AuthHeader from "../components/AuthHeader";
 import Container from "/src/ui/Container";
 import Flex from "/src/ui/Flex";
 import Box from "/src/ui/Box";
+import Paragraph from "/src/ui/Paragraph";
+import Button from "/src/ui/Button";
 
 export default function SignUp() {
   return (
@@ -14,7 +16,16 @@ export default function SignUp() {
           classname={"rounded-xl border border-stone-300 p-6 shadow-lg"}
         >
           <AuthHeader />
-          <h1>Sign up to your account</h1>
+          {/* form */}
+
+          <Flex variant="center" classname={"mt-4 gap-2"}>
+            <Paragraph classname={"text-sm whitespace-nowrap text-slate-400"}>
+              Already have an account?
+            </Paragraph>
+            <Button to="/sign-in" classname={"text-[0.8rem] font-semibold"}>
+              Log in
+            </Button>
+          </Flex>
         </Box>
       </Flex>
     </Container>
