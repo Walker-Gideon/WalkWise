6001cded269abc755f04e4b9956a2d9f212be664
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-09 21:47:44 +0000

    Update the sign up component with the layout

--- src/authentication/sign-up/SignUp.jsx
+++ src/authentication/sign-up/SignUp.jsx
@@ -1,15 +1,22 @@
-import Container from "/src/ui/Container";
 import AuthCloseButton from "../components/AuthCloseButton";
 import AuthHeader from "../components/AuthHeader";
+import Container from "/src/ui/Container";
+import Flex from "/src/ui/Flex";
+import Box from "/src/ui/Box";
 
 export default function SignUp() {
   return (
     <Container>
       <AuthCloseButton />
-      <Container adjust={true}>
-        <AuthHeader />
-        <h1>Sign up to your account</h1>
-      </Container>
+      <Flex variant="center" classname={"min-h-[90vh]"}>
+        <Box
+          adjustWidth={true}
+          classname={"rounded-xl border border-stone-300 p-6 shadow-lg"}
+        >
+          <AuthHeader />
+          <h1>Sign up to your account</h1>
+        </Box>
+      </Flex>
     </Container>
   );
 }
