536a409f30f6a7fe37a86e688d3b6f59be119442
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-09 21:17:36 +0000

    Added the close authentication button in the auth sign up component

--- src/authentication/sign-up/SignUp.jsx
+++ src/authentication/sign-up/SignUp.jsx
@@ -1,7 +1,12 @@
+import AuthCloseButton from "../components/AuthCloseButton";
+
 export default function SignUp() {
   return (
     <div>
-      <h1>Sign up to your account</h1>
+      <div className="">
+        <AuthCloseButton />
+        <h1>Sign up to your account</h1>
+      </div>
     </div>
   );
 }
