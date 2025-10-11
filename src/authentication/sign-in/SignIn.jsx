57dc82b0201e6ababb82a7160354760eeb42c960
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-11 22:01:41 +0000

    Set the layout for the sign in component

--- src/authentication/sign-in/SignIn.jsx
+++ src/authentication/sign-in/SignIn.jsx
@@ -3,14 +3,25 @@ import AuthHeader from "../components/AuthHeader";
 import Container from "/src/ui/Container";
 import Flex from "/src/ui/Flex";
 import Box from "/src/ui/Box";
-import Paragraph from "/src/ui/Paragraph";
 import Button from "/src/ui/Button";
 
 export default function SignIn() {
   return (
     <Container>
       <AuthCloseButton />
-      <h1>Sign up to your account</h1>
+      <Flex variant="center" classname={"min-h-[90vh]"}>
+        <Box
+          adjustWidth={true}
+          classname={"rounded-xl border border-stone-300 p-6 shadow-lg"}
+        >
+          <AuthHeader />
+          {/* Sign-in form */}
+          <Flex variant="between" classname={"mt-4"}>
+            <Button to="/forgotten">Forget Password?</Button>
+            <Button to="/sign-up">Sign Up</Button>
+          </Flex>
+        </Box>
+      </Flex>
     </Container>
   );
 }
