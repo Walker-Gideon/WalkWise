d2a93f5e23b3d737320b5e066242a7050e16a0c4
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-11 21:52:19 +0000

    Update the SignIn component with the auth-colse button

--- src/authentication/sign-in/SignIn.jsx
+++ src/authentication/sign-in/SignIn.jsx
@@ -1,7 +1,16 @@
+import AuthCloseButton from "../components/AuthCloseButton";
+import AuthHeader from "../components/AuthHeader";
+import Container from "/src/ui/Container";
+import Flex from "/src/ui/Flex";
+import Box from "/src/ui/Box";
+import Paragraph from "/src/ui/Paragraph";
+import Button from "/src/ui/Button";
+
 export default function SignIn() {
   return (
-    <div>
+    <Container>
+      <AuthCloseButton />
       <h1>Sign up to your account</h1>
-    </div>
+    </Container>
   );
 }
