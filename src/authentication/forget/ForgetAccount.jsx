4b5e49288cb57fa6f86fa1ea9f1b449d97f03f33
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-13 23:57:28 +0000

    Added the forget component text

--- src/authentication/forget/ForgetAccount.jsx
+++ src/authentication/forget/ForgetAccount.jsx
@@ -1,7 +1,25 @@
+import Container from "../../ui/Container";
+import Flex from "../../ui/Flex";
+import Box from "../../ui/Box";
+
 export default function ForgetAccount() {
   return (
-    <div>
-      <h1>Forget your account?</h1>
-    </div>
+    <>
+      <Flex variant="center" classname={"min-h-[95vh] flex-col"}>
+        <Box adjustWidth={true} classname={"medium:max-w-3xl px-14"}>
+          <Box adjustWidth={true} classname={"medium:text-base mb-6 text-sm"}>
+            <h1 className="medium:text-2xl middle:text-3xl mt-2 mb-2 text-xl font-bold">
+              Reset your password
+            </h1>
+
+            <p>
+              Enter the email you signed up with. We'll send you a link to log
+              in and reset your password. If you signed up with your parent’s
+              email, we’ll send them a link.
+            </p>
+          </Box>
+        </Box>
+      </Flex>
+    </>
   );
 }
