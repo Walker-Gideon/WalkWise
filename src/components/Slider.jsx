5637a8db5ceedd58b9f50f4ba1a3ff80662cb28f
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-16 07:42:49 +0000

    Adjustment to the slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,23 @@
-import Heading from "/src/ui/Heading";
 import MainNav from "/src/navigation/MainNav";
+import Logo from "/src/components/Logo";
+import Header from "/src/ui/Header";
 import Aside from "/src/ui/Aside";
+import Box from "/src/ui/Box";
 
 export default function Slider() {
   return (
     <Aside classname="borderStyling border-r w-[20%]">
-      <Heading />
+      <Header
+        classname={
+          "borderStyling border-b p-4 flex items-center justify-center"
+        }
+      >
+        <Box // dark:bg-slate-700
+          classname={"rounded-sm bg-slate-50 p-2"}
+        >
+          <Logo />
+        </Box>
+      </Header>
       <MainNav />
     </Aside>
   );
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,18 +1,10 @@
-import Logo from "/src/components/Logo";
 import Header from "./Header";
 import Box from "./Box";
 
 export default function Heading() {
   return (
-    <Header
-      classname={"borderStyling border-b p-4 flex items-center justify-center"}
-    >
-      <Box
-        // dark:bg-slate-700
-        classname={"rounded-sm bg-slate-50 p-2"}
-      >
-        <Logo />
-      </Box>
+    <Header>
+      <Box></Box>
     </Header>
   );
 }
