d9063310823f23c78e12f443ced9ecebe3eae356
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-16 07:20:28 +0000

    Update the main nav styling

--- src/navigation/MainNav.jsx
+++ src/navigation/MainNav.jsx
@@ -47,14 +47,19 @@ const buttonsData = [
 
 export default function MainNav() {
   return (
-    <Nav>
+    <Nav classname={"bg-red-500"}>
       {buttonsData.map((data, index) => (
         <UnorderedList
           key={index}
           classname={`flex flex-col gap-2 ${index === 0 || index === 5 ? `` : `py-2`}`}
         >
-          <NavLink to={data.to}>
-            <List classname={"flex items-center py-2"}>
+          <NavLink
+            to={data.to}
+            className={({ isActive }) =>
+              `cursor-pointer rounded-sm px-4 py-2 text-sm font-semibold text-slate-800 transition-all duration-300 hover:bg-slate-600 hover:text-white ${isActive ? "bg-slate-500 text-white" : ""}`
+            }
+          >
+            <List classname={"flex items-center gap-2.5"}>
               <SpanText>{<data.icon />}</SpanText>
               <SpanText>{data.text}</SpanText>
             </List>
