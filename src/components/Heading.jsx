b4cb24009631b3b273e088b46c2019edcf09f7c8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 06:53:15 +0000

    Adjust the styling in the heading component

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -7,12 +7,19 @@ import Flex from "/src/ui/Flex";
 export default function Heading({ children, classname }) {
   return (
     <Header
-      classname={`medium:block sticky top-0 z-40 hidden border-b border-stone-300 bg-white/30 shadow-sm backdrop-blur-sm dark:border-slate-700 dark:bg-slate-800/80 ${classname}`}
+      // dark:border-slate-700 dark:bg-slate-800/80 medium:block hidden
+      classname={`sticky top-0 z-40 border-b border-stone-300 bg-white/30 shadow-sm backdrop-blur-sm  ${classname}`}
     >
       <Flex variant="between" classname={"px-6 py-4"}>
         <Group>
-          <HeaderText></HeaderText>
-          <Paragraph></Paragraph>
+          <HeaderText //dark:text-white
+            classname={"text-xl font-bold text-slate-900"}
+          >
+            Welcome,&nbsp; username
+          </HeaderText>
+          <Paragraph classname={"text-sm text-slate-500"}>
+            Glad to have you on board.
+          </Paragraph>
         </Group>
         <Group>{children}</Group>
       </Flex>
