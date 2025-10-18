625856c716059b48f92515506d873c9ebbde31e4
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 06:39:36 +0000

    Update the heading component with the headertext and paragraph

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,3 +1,5 @@
+import HeaderText from "/src/ui/HeaderText";
+import Paragraph from "/src/ui/Paragraph";
 import Header from "/src/ui/Header";
 import Group from "/src/ui/Group";
 import Flex from "/src/ui/Flex";
@@ -7,10 +9,12 @@ export default function Heading({ children, classname }) {
     <Header
       classname={`medium:block sticky top-0 z-40 hidden border-b border-stone-300 bg-white/30 shadow-sm backdrop-blur-sm dark:border-slate-700 dark:bg-slate-800/80 ${classname}`}
     >
-      <Flex classname={"px-6 py-4"}>
-        <Group></Group>
-        <Group></Group>
-        {children}
+      <Flex variant="between" classname={"px-6 py-4"}>
+        <Group>
+          <HeaderText></HeaderText>
+          <Paragraph></Paragraph>
+        </Group>
+        <Group>{children}</Group>
       </Flex>
     </Header>
   );
--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
 import Container from "/src/ui/Container";
-import Heading from "/src/ui/Heading";
+import Heading from "/src/components/Heading";
 
 export default function Dashboard() {
   return (
-    <Container classname={"bg-red-700"}>
+    <Container classname={""}>
       <Heading></Heading>
       <h1>Dashboard</h1>
     </Container>
