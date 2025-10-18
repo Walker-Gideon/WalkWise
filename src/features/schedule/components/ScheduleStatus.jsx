4b83b29d19c9343e5f1d91badc5f64479d949e1e
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 10:28:06 +0000

    Update in the schedule status

--- src/features/schedule/components/ScheduleStatus.jsx
+++ src/features/schedule/components/ScheduleStatus.jsx
@@ -1,4 +1,9 @@
 import { LuTarget, LuClock, LuFlame, LuTrendingUp } from "react-icons/lu";
+import HeaderText from "/src/ui/HeaderText";
+import Badge from "/src/components/Badge";
+import Paragraph from "/src/ui/Paragraph";
+import Card from "/src/components/Card";
+import Group from "/src/ui/Group";
 
 const status = [
   {
@@ -24,5 +29,34 @@ const status = [
 ];
 
 export default function ScheduleStatus() {
-  return <div></div>;
+  return (
+    <Group
+      classname={
+        "medium:grid-cols-2 grid grid-cols-1 gap-4 medium:gap-6 lg:grid-cols-4"
+      }
+    >
+      {status.map((stats, index) => (
+        <Card
+          key={index}
+          classname={"hover:shadow-lg flex items-center justify-between"}
+        >
+          <Badge type="primary">
+            <stats.icon
+              // dark:text-slate-300
+              className={`h-5 w-5 text-slate-600`}
+            />
+          </Badge>
+          <Group classname="text-right">
+            <HeaderText type="primary">{stats.data}</HeaderText>
+            <Paragraph
+              variant="small"
+              classname="text-nowrap dark:text-slate-400"
+            >
+              {stats.text}
+            </Paragraph>
+          </Group>
+        </Card>
+      ))}
+    </Group>
+  );
 }
