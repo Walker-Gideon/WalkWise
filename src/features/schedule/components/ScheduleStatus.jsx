a7959b791d8bb06a932e5c7fc693a239fa4fbe67
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 10:20:40 +0000

    Set the layout for the schedul status

--- src/features/schedule/components/ScheduleStatus.jsx
+++ src/features/schedule/components/ScheduleStatus.jsx
@@ -1,4 +1,27 @@
-import React from "react";
+import { LuTarget, LuClock, LuFlame, LuTrendingUp } from "react-icons/lu";
+
+const status = [
+  {
+    icon: LuTarget,
+    data: 0,
+    text: "Cards Today",
+  },
+  {
+    icon: LuClock,
+    data: 0 + "m",
+    text: "Study Time",
+  },
+  {
+    icon: LuFlame,
+    data: 0,
+    text: "Day Streak",
+  },
+  {
+    icon: LuTrendingUp,
+    data: 0 + "%",
+    text: "Success Rate",
+  },
+];
 
 export default function ScheduleStatus() {
   return <div></div>;
