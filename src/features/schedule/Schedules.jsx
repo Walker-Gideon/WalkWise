23acf2295b8741aa06b8ad343e4b2308d042e621
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 10:21:45 +0000

    Update the schedule with schedule status

--- src/features/schedule/Schedules.jsx
+++ src/features/schedule/Schedules.jsx
@@ -1,11 +1,18 @@
 import ScheduleHeader from "./components/ScheduleHeader";
+import ScheduleStatus from "./components/ScheduleStatus";
 import Container from "/src/ui/Container";
+import Main from "/src/ui/Main";
 
 export default function Schedules() {
   return (
     <Container>
       <ScheduleHeader />
-      <h1>Schedules</h1>
+      <Main
+        // medium:mt-0 mt-7
+        classname={"h-screen space-y-6 overflow-scroll p-6"}
+      >
+        <ScheduleStatus />
+      </Main>
     </Container>
   );
 }
