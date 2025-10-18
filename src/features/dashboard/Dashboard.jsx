ca6a123ff1de4303584e935cc6597df89a378ed8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 08:59:31 +0000

    Created the dashboard status component and update teh dashboard layout

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,11 +1,18 @@
 import DashboardHeader from "./components/DashboardHeader";
+import DashboardStatus from "./components/DashboardStatus";
 import Container from "/src/ui/Container";
+import Main from "/src/ui/Main";
 
 export default function Dashboard() {
   return (
     <Container classname={""}>
       <DashboardHeader />
-      <h1>Dashboard</h1>
+      <Main
+        // medium:mt-0 mt-7
+        classname={"h-screen space-y-6 overflow-scroll p-6"}
+      >
+        <DashboardStatus />
+      </Main>
     </Container>
   );
 }
