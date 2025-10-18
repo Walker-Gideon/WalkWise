cf501cfad778286a44789d9b40c3164ca8c20633
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-18 09:19:12 +0000

    Adjustment to the dashboard status styling

--- src/features/dashboard/components/DashboardStatus.jsx
+++ src/features/dashboard/components/DashboardStatus.jsx
@@ -4,6 +4,7 @@ import {
   LuClock,
   LuFlame,
 } from "react-icons/lu";
+import Group from "/src/ui/Group";
 
 const status = [
   {
@@ -30,29 +31,32 @@ const status = [
 
 export default function DashboardStatus() {
   return (
-    <div className="medium:grid-cols-2 grid grid-cols-1 gap-6 lg:grid-cols-4">
+    <Group classname="medium:grid-cols-2 grid grid-cols-1 gap-4 medium:gap-6 lg:grid-cols-4">
       {status.map((stats, index) => (
-        <div className="" key={index}>
-          {/* shadow */}
-          <div className="rounded-2xl border border-stone-300 bg-white/70 p-6 backdrop-blur-xl transition-all duration-300 hover:shadow-lg dark:border-slate-700 dark:bg-slate-800/70">
-            <div className="mb-2 flex items-center justify-between">
-              <div className="rounded-xl bg-gradient-to-r from-slate-200 to-slate-300 p-3 dark:from-slate-600 dark:to-slate-700">
-                <stats.icon
-                  className={`h-5 w-5 text-slate-600 dark:text-slate-300`}
-                />
-              </div>
-              <div className="text-right">
-                <p className="medium:text-xl text-lg font-bold text-slate-900 dark:text-white">
-                  {stats.data}
-                </p>
-                <p className="medium:text-sm text-xs text-slate-500 dark:text-slate-400">
-                  {stats.text}
-                </p>
-              </div>
+        /* dark:border-slate-700 dark:bg-slate-800/70 */
+        <div
+          key={index}
+          className="rounded-2xl border border-stone-300 bg-white/70 p-6 backdrop-blur-xl transition-all duration-300 hover:shadow-lg"
+        >
+          <div className="mb-2 flex items-center justify-between">
+            <div className="rounded-xl bg-gradient-to-r from-slate-200 to-slate-300 p-3 dark:from-slate-600 dark:to-slate-700">
+              <stats.icon
+                className={`h-5 w-5 text-slate-600 dark:text-slate-300`}
+              />
+            </div>
+            <div className="text-right">
+              <p //dark:text-white
+                className="medium:text-xl text-lg font-bold text-slate-900"
+              >
+                {stats.data}
+              </p>
+              <p className="medium:text-sm text-xs text-nowrap text-slate-500 dark:text-slate-400">
+                {stats.text}
+              </p>
             </div>
           </div>
         </div>
       ))}
-    </div>
+    </Group>
   );
 }
