85a7a09cd994e99ea1943a755e50eaa935802b91
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-19 12:25:17 +0000

    Update in the dashboard calendar component

--- src/features/dashboard/components/DashboardCalendar.jsx
+++ src/features/dashboard/components/DashboardCalendar.jsx
@@ -1,6 +1,26 @@
 import { LuCalendar } from "react-icons/lu";
 import Card from "/src/components/Card";
+import SpanText from "/src/ui/SpanText";
+import HeaderText from "/src/ui/HeaderText";
+import Group from "/src/ui/Group";
+import useCalendar from "/src/hook/useCalendar";
 
 export default function DashboardCalendar() {
-  return <Card></Card>;
+  const { currentMonth, monthLabel, calendarDays } = useCalendar();
+
+  return (
+    <Card>
+      <HeaderText
+        type="secondary"
+        classname={"mb-4 flex items-center space-x-2"}
+      >
+        <LuCalendar
+          // dark:text-slate-400
+          className="h-5 w-5 text-slate-600"
+        />
+        <SpanText>{monthLabel}</SpanText>
+      </HeaderText>
+      <Group classname={""}></Group>
+    </Card>
+  );
 }
