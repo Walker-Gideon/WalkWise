52be04164b8f795bc9f99023e55f2a7c7ff69d12
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-19 00:39:21 +0000

    Set the layout for the action component

--- src/features/dashboard/components/DashboardActions.jsx
+++ src/features/dashboard/components/DashboardActions.jsx
@@ -1,7 +1,34 @@
 import actions from "/src/data/dashboardActionsData";
 
 export default function DashboardActions() {
-  return <div>
-    {actions.map((action, index) => ())}
-  </div>;
+  // const navigate = useLoaderAction(1000);
+  return (
+    <div>
+      <h3 className="mb-4 text-lg font-semibold text-slate-900 dark:text-white">
+        Quick Actions
+      </h3>
+
+      <div className="grid grid-cols-2 gap-4 lg:grid-cols-4">
+        {actions.map((action, index) => (
+          <button
+            key={index}
+            /*   onClick={() => {
+              navigate(data.to);
+              setTimeout(() => {
+                setNavigateTitle(data.title);
+              }, 1000);
+            }} */
+            className="group flex cursor-pointer flex-col items-center space-y-3 rounded-xl bg-slate-50 p-6 transition-all duration-300 hover:scale-105 hover:bg-slate-100 hover:shadow-md dark:bg-slate-700/50 dark:hover:bg-slate-700"
+          >
+            <div className="rounded-xl bg-slate-300 p-3 transition-colors group-hover:bg-slate-200 dark:bg-slate-500">
+              <action.icon className="h-5 w-5 text-slate-600 dark:text-slate-400" />
+            </div>
+            <span className="medium:text-sm text-xs font-medium text-slate-700 dark:text-slate-300">
+              {action.text}
+            </span>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
