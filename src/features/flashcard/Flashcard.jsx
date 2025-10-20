80076b479515407226ca0c7f93d4b3477bfc904c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-20 23:45:17 +0000

    Adjustment in the flashcard component

--- src/features/flashcard/Flashcard.jsx
+++ src/features/flashcard/Flashcard.jsx
@@ -1,10 +1,13 @@
-import FlashcardHeader from "./components/FlashcardHeader";
+import HeaderText from "/src/ui/HeaderText";
 import Container from "/src/ui/Container";
+import Header from "/src/ui/Header";
 
 export default function Flashcard() {
   return (
     <Container>
-      <FlashcardHeader />
+      <Header>
+        <HeaderText variant="header">My Flashcards</HeaderText>
+      </Header>
       <h1>Flashcard</h1>
     </Container>
   );
--- src/features/flashcard/components/FlashcardHeader.jsx
+++ src/features/flashcard/components/FlashcardHeader.jsx
@@ -1,10 +1,3 @@
-import HeaderText from "/src/ui/HeaderText";
-import Header from "/src/ui/Header";
-
 export default function FlashcardHeader() {
-  return (
-    <Header>
-      <HeaderText variant="header">My Flashcards</HeaderText>
-    </Header>
-  );
+  return <div className="">FlashcardHeader</div>;
 }
