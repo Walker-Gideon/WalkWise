f0550038e403cd5740edc8d4163814496c542919
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-20 23:41:48 +0000

    Update the flashcard header component

--- src/features/flashcard/components/FlashcardHeader.jsx
+++ src/features/flashcard/components/FlashcardHeader.jsx
@@ -1,5 +1,16 @@
+import HeaderText from "/src/ui/HeaderText";
 import Header from "/src/ui/Header";
 
 export default function FlashcardHeader() {
-  return <Header>FlashcardHeader</Header>;
+  return (
+    <Header>
+      <HeaderText
+        classname={
+          "text-xl font-bold text-slate-900 dark:text-white medium:mb-2 medium:block hidden"
+        }
+      >
+        My Flashcards
+      </HeaderText>
+    </Header>
+  );
 }
