80a9b73481fd6ca69edbd4086227200ec1dbb8b0
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-21 01:21:48 +0000

    Update the flashcard component

--- src/features/flashcard/Flashcard.jsx
+++ src/features/flashcard/Flashcard.jsx
@@ -1,5 +1,6 @@
+import { LuRectangleVertical } from "react-icons/lu";
+import InformationPrompt from "../../components/InformationPrompt";
 import FlashcardHeader from "./components/FlashcardHeader";
-import FlashcardPrompt from "./components/FlashcardPrompt";
 import Container from "/src/ui/Container";
 import Main from "/src/ui/Main";
 
@@ -10,7 +11,14 @@ export default function Flashcard() {
 
       {/* will adjust the height base on the state that display the component */}
       <Main classname={"h-[400px]"}>
-        <FlashcardPrompt />
+        <InformationPrompt
+          /* dark:text-slate-300 */
+          icon={<LuRectangleVertical className="icons" />}
+          promptText="You haven't created any flashcards yet."
+          actionText='Get started by tapping "Create Flashcard"'
+          onclick={() => {}}
+          buttonText="Create Flashcard"
+        />
       </Main>
     </Container>
   );
