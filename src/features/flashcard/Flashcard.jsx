144b60fe23cb192f4bb37b8500d54a7058838273
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-21 01:39:42 +0000

    Set the logic to display either the create flashcard layout or the inital flashcard  display

--- src/features/flashcard/Flashcard.jsx
+++ src/features/flashcard/Flashcard.jsx
@@ -1,24 +1,40 @@
 import { LuRectangleVertical } from "react-icons/lu";
 import InformationPrompt from "../../components/InformationPrompt";
 import FlashcardHeader from "./components/FlashcardHeader";
+import Conditional from "/src/components/Conditional";
 import Container from "/src/ui/Container";
 import Main from "/src/ui/Main";
+import { useState } from "react";
+import CreateFlashcardLayout from "./components/create-cards/CreateFlashcardLayout";
 
 export default function Flashcard() {
+  const [isDisplayCreateCard, setIsDisplayCreateCard] = useState(false);
+
+  function handleDisplayCreateCard() {
+    setTimeout(() => {
+      setIsDisplayCreateCard((show) => !show);
+    }, 200);
+  }
+
   return (
     <Container>
-      <FlashcardHeader />
-
-      {/* will adjust the height base on the state that display the component */}
-      <Main classname={"h-[400px]"}>
-        <InformationPrompt
-          /* dark:text-slate-300 */
-          icon={<LuRectangleVertical className="icons" />}
-          promptText="You haven't created any flashcards yet."
-          actionText='Get started by tapping "Create Flashcard"'
-          onclick={() => {}}
-          buttonText="Create Flashcard"
-        />
+      <FlashcardHeader isDisplayCreateCard={isDisplayCreateCard} />
+      <Main
+        classname={`${isDisplayCreateCard ? "h-screen w-full" : "h-[400px]"}`}
+      >
+        <Conditional condition={!isDisplayCreateCard}>
+          <InformationPrompt
+            /* dark:text-slate-300 */
+            icon={<LuRectangleVertical className="icons" />}
+            promptText="You haven't created any flashcards yet."
+            actionText='Get started by tapping "Create Flashcard"'
+            onclick={handleDisplayCreateCard}
+            buttonText="Create Flashcard"
+          />
+        </Conditional>
+        <Conditional condition={isDisplayCreateCard}>
+          <CreateFlashcardLayout />
+        </Conditional>
       </Main>
     </Container>
   );
--- src/features/flashcard/components/FlashcardHeader.jsx
+++ src/features/flashcard/components/FlashcardHeader.jsx
@@ -1,9 +1,9 @@
 import HeaderText from "/src/ui/HeaderText";
 import Header from "/src/ui/Header";
 
-export default function FlashcardHeader() {
+export default function FlashcardHeader({ isDisplayCreateCard }) {
   return (
-    <Header type="padding">
+    <Header type="padding" classname={`${isDisplayCreateCard ? "hidden" : ""}`}>
       <HeaderText variant="header">My Flashcards</HeaderText>
     </Header>
   );
