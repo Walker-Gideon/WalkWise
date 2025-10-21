0a4456fdc59d6e8676a53bbf15b2fadd763ec57c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-21 00:10:53 +0000

    Adjustment in the badge component and update in the flascard component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,12 +1,9 @@
 export default function Badge({ children, type, classname }) {
+  const base = "rounded-xl";
   const styling = {
     // dark:from-slate-600 dark:to-slate-700
-    primary: "bg-gradient-to-r from-slate-200 to-slate-300",
-    secondary: "bg-slate-300 group-hover:bg-slate-200", //dark:bg-slate-500
+    primary: base + " bg-gradient-to-r from-slate-200 to-slate-300",
+    secondary: base + " bg-slate-300 group-hover:bg-slate-200", //dark:bg-slate-500
   };
-  return (
-    <div className={`rounded-xl p-3 ${styling[type]} ${classname}`}>
-      {children}
-    </div>
-  );
+  return <div className={`p-3 ${styling[type]} ${classname}`}>{children}</div>;
 }
--- src/features/flashcard/Flashcard.jsx
+++ src/features/flashcard/Flashcard.jsx
@@ -1,8 +1,11 @@
+import { LuRectangleVertical } from "react-icons/lu";
 import HeaderText from "/src/ui/HeaderText";
 import Container from "/src/ui/Container";
 import Header from "/src/ui/Header";
 import Button from "/src/ui/Button";
+import Badge from "/src/components/Badge";
 import Main from "/src/ui/Main";
+import Box from "/src/ui/Box";
 
 export default function Flashcard() {
   return (
@@ -10,8 +13,16 @@ export default function Flashcard() {
       <Header>
         <HeaderText variant="header">My Flashcards</HeaderText>
       </Header>
-      <Main>
-        <Button>Create Flashcard</Button>
+      <Main classname={"h-[560px]"}>
+        <Box
+          adjustWidth={true}
+          classname={"h-full w-full flex items-center justify-center flex-col"}
+        >
+          <Badge classname={"bg-amber-300 rounded-full"}>
+            <LuRectangleVertical className="h-6 w-6 text-slate-600 dark:text-slate-300" />
+          </Badge>
+          <Button>Create Flashcard</Button>
+        </Box>
       </Main>
     </Container>
   );
