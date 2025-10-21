6739e719854665ba46c40205da166719727b5514
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-21 01:10:14 +0000

    set the layout for the information prompt component

--- src/components/InformationPrompt.jsx
+++ src/components/InformationPrompt.jsx
@@ -1,3 +1,35 @@
+import { LuRectangleVertical } from "react-icons/lu";
+import HeaderText from "/src/ui/HeaderText";
+import Paragraph from "/src/ui/Paragraph";
+import Badge from "/src/components/Badge";
+import Button from "/src/ui/Button";
+import Group from "/src/ui/Group";
+
 export default function InformationPrompt() {
-  return <div></div>;
+  return (
+    <Group
+      classname={"h-full w-full flex items-center justify-center flex-col"}
+    >
+      {/* text-slate-500 dark:text-slate-400 */}
+      <Badge type="icons">
+        {/* dark:text-slate-300 */}
+        <LuRectangleVertical className="icons" />
+      </Badge>
+      <Group classname={"text-center py-2"}>
+        <HeaderText variant="secondary">
+          You haven't created any flashcards yet.
+        </HeaderText>
+        <Paragraph type="information">
+          Get started by tapping "Create Flashcard"
+        </Paragraph>
+      </Group>
+      <Button
+        type="colors"
+        onclick={() => {}}
+        classname={"focus:ring-slate-300"}
+      >
+        Create Flashcard
+      </Button>
+    </Group>
+  );
 }
