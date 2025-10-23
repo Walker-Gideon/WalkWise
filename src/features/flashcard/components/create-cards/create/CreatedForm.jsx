f267063039665ae954a74ec33f13939785943f3e
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-23 11:27:08 +0000

    Update the Created form component

--- src/features/flashcard/components/create-cards/create/CreatedForm.jsx
+++ src/features/flashcard/components/create-cards/create/CreatedForm.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import CreatedHeader from "./CreatedHeader";
+import Form from "/src/ui/Form";
+import Box from "/src/ui/Box";
 
 export default function CreatedForm() {
-  return <div></div>;
+  return (
+    <>
+      <CreatedHeader />
+      <Box
+        adjustWidth={true}
+        classname={
+          "mx-auto medium:mt-8 mt-14 max-w-3xl maxmid:max-w-4xl medium:h-[78vh] overflow-y-scroll"
+        }
+      >
+        <Form onsubmit={() => {}}></Form>
+      </Box>
+    </>
+  );
 }
