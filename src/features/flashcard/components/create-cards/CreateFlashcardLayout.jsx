35ce14b36201cf60da140216bc9991d8a74759eb
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-23 00:04:51 +0000

    Update the createdflashcardlayout component

--- src/features/flashcard/components/create-cards/CreateFlashcardLayout.jsx
+++ src/features/flashcard/components/create-cards/CreateFlashcardLayout.jsx
@@ -1,12 +1,28 @@
 import HeaderText from "/src/ui/HeaderText";
+import Container from "/src/ui/Container";
 import Header from "/src/ui/Header";
+import Group from "/src/ui/Group";
 
 export default function CreateFlashcardLayout() {
   return (
-    <div>
+    <Container adjust={true}>
       <Header type="padding" classname={""}>
-        <HeaderText variant="header">Create a new flashcard set</HeaderText>
+        <HeaderText variant="header">Create A New flashcard Set</HeaderText>
       </Header>
-    </div>
+
+      <Group classname="mx-auto medium:mt-8 mt-14 max-w-3xl maxmid:max-w-4xl medium:h-[70vh] bg-red-500">
+        <form className="space-y-2">
+          {/* input tag */}
+
+          {/* add flashcard button */}
+
+          {/* optional tag for set name */}
+
+          {/* action button */}
+
+          {/* notify information */}
+        </form>
+      </Group>
+    </Container>
   );
 }
