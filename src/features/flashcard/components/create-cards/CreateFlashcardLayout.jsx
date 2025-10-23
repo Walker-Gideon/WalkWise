91031b39af31c84032343f12675a0f46dbb86738
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-23 10:36:05 +0000

    Update the form with the states

--- src/features/flashcard/components/create-cards/CreateFlashcardLayout.jsx
+++ src/features/flashcard/components/create-cards/CreateFlashcardLayout.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { RiDeleteBin5Line } from "react-icons/ri";
 
 import Heading from "/src/components/Heading";
@@ -12,6 +13,14 @@ import Flex from "/src/ui/Flex";
 import Box from "/src/ui/Box";
 
 export default function CreateFlashcardLayout() {
+  const [pairs, setPairs] = useState([
+    { term: "", definition: "" },
+    { term: "", definition: "" },
+  ]);
+  const [index, setIndex] = useState(0);
+
+  const MAX_PAIRS = 100;
+
   const styling = {
     label:
       "mb-1 block medium:text-xs text-sm font-medium text-slate-500 dark:text-slate-400",
@@ -37,7 +46,7 @@ export default function CreateFlashcardLayout() {
       <Box
         adjustWidth={true}
         classname={
-          "mx-auto medium:mt-8 mt-14 max-w-3xl maxmid:max-w-4xl medium:h-[70vh]"
+          "mx-auto medium:mt-8 mt-14 max-w-3xl maxmid:max-w-4xl medium:h-[78vh] overflow-y-scroll"
         }
       >
         <Form onsubmit={() => {}}>
@@ -65,52 +74,74 @@ export default function CreateFlashcardLayout() {
             classname={"space-y-6 py-4"}
           >
             {/* Will map here to add more input tag */}
-            <Group
-              classname={
-                "flex flex-col gap-4 rounded-lg bg-slate-50 p-4 dark:bg-slate-600"
-              }
-            >
-              <Group>
-                <Flex variant="between" classname={"w-full"}>
+            {pairs.map((pair, idx) => (
+              <Group
+                key={idx}
+                classname={
+                  "flex flex-col gap-4 rounded-lg bg-slate-50 p-4 dark:bg-slate-600"
+                }
+              >
+                <Group>
+                  <Flex variant="between" classname={"w-full mb-2"}>
+                    <Label htmlfor={`term-${idx}`} classname={styling.label}>
+                      Term{" "}
+                      {pairs.length > 2
+                        ? `#${idx + 1}`
+                        : idx === 0
+                          ? "#1"
+                          : "#2"}
+                    </Label>
+                    <Button classname={"bg-red-500"} onclick={() => {}}>
+                      <RiDeleteBin5Line className="icons text-slate-500 dark:text-slate-200" />
+                    </Button>
+                  </Flex>
+                  <Input
+                    id={``}
+                    name={``}
+                    type="text"
+                    // value={}
+                    // onChange={}
+                    placeholder="Enter term..."
+                    classname={styling.inputArea}
+                    // disabled={}
+                  />
+                </Group>
+                <Group>
                   <Label htmlfor={``} classname={styling.label}>
-                    Term
+                    Definition
                   </Label>
-                  <Button>
-                    <RiDeleteBin5Line className="icons" />
-                  </Button>
-                </Flex>
-                <Input
-                  id={``}
-                  name={``}
-                  type="text"
-                  // value={}
-                  // onChange={}
-                  placeholder="Enter term..."
-                  classname={styling.inputArea}
-                  // disabled={}
-                />
+                  <TextArea
+                    id={``}
+                    name={``}
+                    rows={2}
+                    // value={}
+                    // onChange={}
+                    classname={styling.inputArea}
+                    placeholder="Enter definition..."
+                    // disabled={}
+                  />
+                </Group>
               </Group>
-              <Group>
-                <Label htmlfor={``} classname={styling.label}>
-                  Definition
-                </Label>
-                <TextArea
-                  id={``}
-                  name={``}
-                  rows={2}
-                  // value={}
-                  // onChange={}
-                  classname={styling.inputArea}
-                  placeholder="Enter definition..."
-                  // disabled={}
-                />
-              </Group>
-            </Group>
+            ))}
           </Container>
 
           {/* add flashcard button */}
           <Flex classname={"items-center justify-end"}>
-            <Button type="colors" classname={"px-8"} onclick={() => {}}>
+            <Button
+              type="colors"
+              classname={"px-8"}
+              onclick={() => {
+                /*
+              const currentPairs = editMode ? editPairs : pairs;
+    const newPair = { term: "", definition: "" };
+
+    if (currentPairs.length < MAX_PAIRS) {
+      const updated = [...currentPairs, newPair];
+      editMode ? setEditPairs(updated) : setPairs(updated);
+    }
+              */
+              }}
+            >
               Add Card
             </Button>
           </Flex>
