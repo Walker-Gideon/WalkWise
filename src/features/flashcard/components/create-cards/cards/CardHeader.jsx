ae9debc91349643be11c80beaff6387c44a8b0fd
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-25 01:19:59 +0000

    set the layout for the cardheader component

--- src/features/flashcard/components/create-cards/cards/CardHeader.jsx
+++ src/features/flashcard/components/create-cards/cards/CardHeader.jsx
@@ -1,3 +1,18 @@
+import Heading from "/src/components/Heading";
+import Button from "/src/ui/Button";
+import Group from "/src/ui/Group";
+
 export default function CardHeader() {
-  return <div>CardHeader</div>;
+  return (
+    <Heading
+      headerText="My Flashcards"
+      paragraphText="Reinforce your memory, one card at a time."
+    >
+      <Group classname={"space-x-2"}>
+        <Button type="colors" onclick={() => {}}>
+          Create Flashcard
+        </Button>
+      </Group>
+    </Heading>
+  );
 }
