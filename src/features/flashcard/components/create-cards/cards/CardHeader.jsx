6e55a160ec3c51ed9522bd88b2c5b64662c2fdd4
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-25 01:26:32 +0000

    Bug in the flashcard section

--- src/features/flashcard/components/create-cards/cards/CardHeader.jsx
+++ src/features/flashcard/components/create-cards/cards/CardHeader.jsx
@@ -1,15 +1,22 @@
+import { useFlashcard } from "../../../context/FlashcardContext";
+import useToggleDisplay from "/src/hook/useToggleDisplay";
+
 import Heading from "/src/components/Heading";
 import Button from "/src/ui/Button";
 import Group from "/src/ui/Group";
 
 export default function CardHeader() {
+  const { setIsDisplayFlashcardLayout } = useFlashcard();
+
+  const toggleDisplay = useToggleDisplay(setIsDisplayFlashcardLayout);
+
   return (
     <Heading
       headerText="My Flashcards"
       paragraphText="Reinforce your memory, one card at a time."
     >
       <Group classname={"space-x-2"}>
-        <Button type="colors" onclick={() => {}}>
+        <Button type="colors" onclick={toggleDisplay}>
           Create Flashcard
         </Button>
       </Group>
