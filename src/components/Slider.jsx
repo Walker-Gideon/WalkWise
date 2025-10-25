65860120a79dd71f3d325a76a7aa9ac5689ba744
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-25 23:17:58 +0000

    Set the toggle button for the theme button

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,6 @@
+import * as motion from "motion/react-client";
 import { LuSun, LuMoon } from "react-icons/lu";
+
 import MainNav from "/src/navigation/MainNav";
 import Logo from "/src/components/Logo";
 import UserProfile from "./UserProfile";
@@ -6,6 +8,7 @@ import Header from "/src/ui/Header";
 import Button from "/src/ui/Button";
 import Aside from "/src/ui/Aside";
 import Box from "/src/ui/Box";
+import { useState } from "react";
 
 export default function Slider() {
   /*
@@ -30,6 +33,9 @@ export default function Slider() {
     }
   };
   */
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => setIsOn(!isOn);
 
   return (
     <Aside classname="borderStyling border-r h-screen flex flex-col justify-between">
@@ -46,14 +52,66 @@ export default function Slider() {
         <MainNav />
       </Box>
       <Box classname={"borderStyling border-t p-4"}>
-        <Button
-          onclick={() => {}}
+        {/* <LuMoon /> Dark mode */}
+        {/*  
+        <button
+          onclick={toggleSwitch}
           // type="colors"
-          variant="motion"
-          classname={"w-full flex items-center gap-2 text-sm"}
+          // variant="motion"
+          style={{
+            justifyContent: "flex-" + (isOn ? "start" : "end"),
+          }}
+          // classname={"w-full flex items-center gap-2 text-sm"}
+          className={
+            "flex h-5 w-50 cursor-pointer rounded-[50%] bg-red-500 p-4"
+          }
         >
-          <LuMoon /> Dark mode
-        </Button>
+          <motion.div
+            className="toggle-handle"
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: "#9911ff",
+              borderRadius: "50%",
+            }}
+            layout
+            transition={{
+              type: "spring",
+              visualDuration: 0.2,
+              bounce: 0.2,
+            }}
+          />
+        </button>*/}
+        <button
+          className="toggle-container"
+          style={{
+            width: 100,
+            height: 50,
+            backgroundColor: "rgba(66, 153, 225, 0.2)",
+            borderRadius: 50,
+            cursor: "pointer",
+            display: "flex",
+            padding: 10,
+            justifyContent: "flex-" + (isOn ? "start" : "end"),
+          }}
+          onClick={toggleSwitch}
+        >
+          <motion.div
+            className="toggle-handle"
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: "rgb(66, 153, 225)",
+              borderRadius: "50%",
+            }}
+            layout
+            transition={{
+              type: "spring",
+              visualDuration: 0.2,
+              bounce: 0.2,
+            }}
+          />
+        </button>
         <UserProfile />
       </Box>
     </Aside>
