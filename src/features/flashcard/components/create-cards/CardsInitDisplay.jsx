91e7f99c01bb79ff5a06e5cf57d20f33d50b8e22
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-25 10:14:49 +0000

    Added the search bar in the initflashcad

--- src/features/flashcard/components/create-cards/CardsInitDisplay.jsx
+++ src/features/flashcard/components/create-cards/CardsInitDisplay.jsx
@@ -1,10 +1,33 @@
+import { LuSearch } from "react-icons/lu";
+
 import CardContentDisplay from "./cards/CardContentDisplay";
 import CardHeader from "./cards/CardHeader";
+import Input from "/src/ui/Input";
+import Flex from "/src/ui/Flex";
 
 export default function CardsInitDisplay() {
   return (
     <>
       <CardHeader />
+      <Flex
+        classname={
+          "items-end justify-end medium:my-6 medium:max-w-80 middle:max-w-xl relative w-full"
+        }
+      >
+        <Input
+          type="text"
+          name="query"
+          // value={}
+          // onChange={}
+          placeholder="Search flashcards"
+          // onKeyDown={}
+          classname={
+            "medium:right-0 w-full bg-transparent px-2 dark:border-slate-700 dark:text-white dark:placeholder:text-slate-400"
+          }
+          // disabled={}
+        />
+        <LuSearch className="absolute top-2.5 right-2 text-sm text-slate-600 dark:text-slate-300" />
+      </Flex>
       <CardContentDisplay />
     </>
   );
