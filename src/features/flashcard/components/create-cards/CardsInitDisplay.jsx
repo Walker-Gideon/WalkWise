c3eff31629d14c84795a5b382b61eb1ed2ee7163
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-25 10:34:12 +0000

    Adjust the styling for the input

--- src/features/flashcard/components/create-cards/CardsInitDisplay.jsx
+++ src/features/flashcard/components/create-cards/CardsInitDisplay.jsx
@@ -10,8 +10,9 @@ export default function CardsInitDisplay() {
     <>
       <CardHeader />
       <Flex
+        // medium:max-w-80  middle:max-w-xl
         classname={
-          "items-end justify-end medium:my-6 medium:max-w-80 middle:max-w-xl relative w-full"
+          "items-end justify-end medium:my-6 medium:max-w-xl relative w-full ml-auto mr-6"
         }
       >
         <Input
@@ -22,11 +23,13 @@ export default function CardsInitDisplay() {
           placeholder="Search flashcards"
           // onKeyDown={}
           classname={
-            "medium:right-0 w-full bg-transparent px-2 dark:border-slate-700 dark:text-white dark:placeholder:text-slate-400"
+            "w-full bg-transparent" //dark:text-white dark:border-slate-700 dark:placeholder:text-slate-400
           }
           // disabled={}
         />
-        <LuSearch className="absolute top-2.5 right-2 text-sm text-slate-600 dark:text-slate-300" />
+        <LuSearch
+          className="absolute top-2.5 right-2 text-sm text-slate-600" //dark:text-slate-300
+        />
       </Flex>
       <CardContentDisplay />
     </>
