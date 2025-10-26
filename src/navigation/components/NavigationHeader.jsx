cc8a81f13935a2810158fbfae60c60269da9ec12
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-26 14:05:55 +0000

    Set the layout for the menu button

--- src/navigation/components/NavigationHeader.jsx
+++ src/navigation/components/NavigationHeader.jsx
@@ -1,4 +1,8 @@
+import { LuCircleChevronLeft } from "react-icons/lu";
+import { LuCircleChevronRight } from "react-icons/lu";
+
 import Logo from "/src/components/Logo";
+import Button from "/src/ui/Button";
 import Header from "/src/ui/Header";
 import Box from "/src/ui/Box";
 
@@ -7,8 +11,12 @@ export default function NavigationHeader() {
     <Header
       classname={"borderStyling border-b p-4 flex items-center justify-center"}
     >
-      <Box classname={"rounded-sm py-2"}>
+      <Box classname={"rounded-sm py-2 flex items-center justify-between"}>
         <Logo />
+        <Button variant="secondary" classname={""} onclick={() => {}}>
+          <LuCircleChevronLeft className="icons" />
+          {/* <LuCircleChevronRight /> */}
+        </Button>
       </Box>
     </Header>
   );
