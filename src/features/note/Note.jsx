b9fd21476330d8e835b1a0d4db3c3adff3dad1e6
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-28 21:58:46 +0000

    Update the Note component with the sub component layout

--- src/features/note/Note.jsx
+++ src/features/note/Note.jsx
@@ -1,7 +1,12 @@
+import NoteLeftLayout from "./components/note-left/NoteLeftLayout";
+import NoteRightLayout from "./components/note-right/NoteRightLayout";
+import Container from "/src/ui/Container";
+
 export default function Note() {
   return (
-    <div>
-      <h1>Note</h1>
-    </div>
+    <Container>
+      <NoteLeftLayout />
+      <NoteRightLayout />
+    </Container>
   );
 }
