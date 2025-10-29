ccf299585604bb63b35432cfad25ee6e4b0e3840
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-29 22:48:30 +0000

    Update the header componenT

--- src/features/note/components/note-left/LeftNoteHeader.jsx
+++ src/features/note/components/note-left/LeftNoteHeader.jsx
@@ -11,7 +11,7 @@ export default function LeftNoteHeader() {
   return (
     <Header classname={"px-6 py-4 border-b borderStyling"}>
       <HeaderText type="primary">My Note</HeaderText>
-      <Group className="relative my-2">
+      <Group classname={"relative my-2"}>
         <Input
           type="text"
           name="queryNote"
@@ -19,13 +19,17 @@ export default function LeftNoteHeader() {
           // onChange={}
           placeholder="Search note"
           // onKeyDown={}
-          classname={"w-full"}
+          classname={"w-full pl-5"}
           // disabled={}
         />
-        <LuSearch className="absolute top-2.5 left-2 text-sm text-slate-600 dark:text-slate-300" />
+        <LuSearch className="absolute top-2.5 left-1 text-sm text-slate-600 dark:text-slate-300" />
       </Group>
-      <Button type="colors" classname={"w-full flex"} onclick={() => {}}>
-        <LuPlus className="text-base" />
+      <Button
+        type="colors"
+        classname={"w-full flex items-center justify-center gap-0.5"}
+        onclick={() => {}}
+      >
+        <LuPlus className="h-3.5 w-3.5 text-white" />
         New note
       </Button>
     </Header>
