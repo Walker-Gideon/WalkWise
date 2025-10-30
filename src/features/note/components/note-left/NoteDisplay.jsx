e586dd1ae6e791984105ad147c18841139f35ec8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-30 22:49:09 +0000

    Set the layout for the NoteDisplay

--- src/features/note/components/note-left/NoteDisplay.jsx
+++ src/features/note/components/note-left/NoteDisplay.jsx
@@ -1,29 +1,43 @@
-// import { RiDeleteBin5Line } from "react-icons/ri";
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+import HeaderText from "/src/ui/HeaderText";
+import Container from "/src/ui/Container";
+import Paragraph from "/src/ui/Paragraph";
+import Button from "/src/ui/Button";
+import Group from "/src/ui/Group";
 
 export default function NoteDisplay() {
   return (
-    <div className="scroll-container mb-4 h-screen overflow-y-scroll">
-      <div
-        className={`my-1 flex w-full cursor-pointer items-center justify-between gap-2 border-b border-stone-300 transition-colors hover:bg-slate-50 dark:border-slate-700 dark:hover:bg-slate-700`}
+    <Container classname={"mb-4 h-screen overflow-y-scroll"}>
+      <Group
+        //   hover:bg-slate-50 dark:border-slate-700 dark:hover:bg-slate-700
+        classname={`my-1 flex w-full cursor-pointer items-center justify-between gap-2 border-b border-stone-300`}
       >
         <div role="button" onClick={() => {}} className="w-full py-2 pl-4">
-          <h1 className="medium:text-sm mb-1 w-40 truncate overflow-hidden whitespace-nowrap text-slate-900 dark:text-white">
+          <HeaderText
+            variant="secondary"
+            // dark:text-white
+            classname={"w-40 truncate whitespace-nowrap text-slate-900"}
+          >
             title
-          </h1>
-
-          <p className="medium:text-xs text-sm text-slate-500 dark:text-slate-400">
+          </HeaderText>
+          <Paragraph
+            variant="small"
+            classname={""}
+            //dark:text-slate-400
+          >
             time
-          </p>
+          </Paragraph>
         </div>
-
-        {/* <Button
-              variant="outline"
-              onClick={() => handleDelete(id)}
-              classname="text-slate-700 pr-4 dark:text-slate-200"
-            >
-              <RiDeleteBin5Line className="h-5 w-5" />
-            </Button> */}
-      </div>
-    </div>
+        <Button
+          variant="secondary"
+          onclick={() => {}}
+          //   dark:text-slate-200
+          classname={"text-slate-700 pr-4"}
+        >
+          <RiDeleteBin5Line className="h-5 w-5" />
+        </Button>
+      </Group>
+    </Container>
   );
 }
