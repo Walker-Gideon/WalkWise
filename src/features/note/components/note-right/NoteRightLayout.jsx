f5df873b0f60eb672b07c09fd537ad532d4d3676
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-30 23:10:07 +0000

    Set the layout for the NoteRightLayout

--- src/features/note/components/note-right/NoteRightLayout.jsx
+++ src/features/note/components/note-right/NoteRightLayout.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import { LuNotebookText } from "react-icons/lu";
+
+import InformationPrompt from "/src/components/InformationPrompt";
+import Conditional from "/src/components/Conditional";
+import Container from "/src/ui/Container";
+
 export default function NoteRightLayout() {
-  return <div>NoteRightLayout</div>;
+  const [isDisplayNote, setIsDisplayNote] = useState(false);
+
+  return (
+    <Container>
+      <Conditional condition={!isDisplayNote}>
+        <InformationPrompt
+          /* dark:text-slate-300 */
+          icon={<LuNotebookText className="icons" />}
+          promptText="Select a note to view"
+          actionText='Choose a note from the sidebar or tap "Create Note" to make one.'
+          onclick={() => {}}
+          buttonText="Create Note"
+        />
+      </Conditional>
+    </Container>
+  );
 }
