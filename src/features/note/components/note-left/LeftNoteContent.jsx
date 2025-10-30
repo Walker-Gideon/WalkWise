ab6c70d91c160380bc610e6f558f34e635929bab
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-30 21:44:42 +0000

    Set the layout for the LeftNoteContent component

--- src/features/note/components/note-left/LeftNoteContent.jsx
+++ src/features/note/components/note-left/LeftNoteContent.jsx
@@ -1,3 +1,20 @@
+import Container from "/src/ui/Container";
+import InformationPrompt from "/src/components/InformationPrompt";
+// import Conditional from "/src/components/Conditional";
+
 export default function LeftNoteContent() {
-  return <div>LeftNoteContent</div>;
+  return (
+    <Container adjust={true} classname={"bg-red-500 h-full"}>
+      {/* <Conditional condition={!isDisplayFlashcardLayout}> */}
+      <InformationPrompt
+        /* dark:text-slate-300 */
+        // icon={<LuRectangleVertical className="icons" />}
+        promptText="You haven't created any flashcards yet."
+        actionText='Get started by tapping "Create Flashcard"'
+        onclick={() => {}}
+        buttonText="Create Flashcard"
+      />
+      {/* </Conditional> */}
+    </Container>
+  );
 }
