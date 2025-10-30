8dd43b4bbf8acdf19af6e58eecd850bbef6c8dff
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-30 23:26:50 +0000

    Created CreateNoteLayout and update with the subcomponent

---
+++ src/features/note/components/note-right/CreateNoteLayout.jsx
@@ -0,0 +1,11 @@
+import CreateNote from "./CreateNote";
+import CreateNoteHeader from "./CreateNoteHeader";
+
+export default function CreateNoteLayout() {
+  return (
+    <>
+      <CreateNoteHeader />
+      <CreateNote />
+    </>
+  );
+}
