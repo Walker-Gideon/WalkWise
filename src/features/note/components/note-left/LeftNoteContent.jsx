93d472ea7ea0ebf4d3605231037560040020dfa5
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-30 22:04:46 +0000

    Update the LeftNoteContent

--- src/features/note/components/note-left/LeftNoteContent.jsx
+++ src/features/note/components/note-left/LeftNoteContent.jsx
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import { LuNotebookText } from "react-icons/lu";
 
-import Container from "/src/ui/Container";
 import InformationPrompt from "/src/components/InformationPrompt";
-// import Conditional from "/src/components/Conditional";
+import Container from "/src/ui/Container";
+import Conditional from "/src/components/Conditional";
 
 export default function LeftNoteContent() {
+  const [isDisplayCreatedNote, setIsDisplayCreatedNote] = useState(false);
+
   return (
-    <Container adjust={true} classname={"h-110"}>
-      {/* <Conditional condition={!isDisplayFlashcardLayout}> */}
-      <InformationPrompt
-        /* dark:text-slate-300 */
-        icon={<LuNotebookText className="icons" />}
-        actionText="No notes found"
-        btn={true}
-      />
-      {/* </Conditional> */}
+    <Container adjust={true} classname={`h-110`}>
+      <Conditional condition={!isDisplayCreatedNote}>
+        <InformationPrompt
+          /* dark:text-slate-300 */
+          icon={<LuNotebookText className="icons" />}
+          actionText="No notes found"
+          btn={true}
+        />
+      </Conditional>
     </Container>
   );
 }
