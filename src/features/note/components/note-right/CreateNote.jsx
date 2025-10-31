c1ca68fda0b20062ffe2d721b9d13f375ffbb8fc
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-31 17:59:20 +0000

    Adjust the CreateNote component

--- src/features/note/components/note-right/CreateNote.jsx
+++ src/features/note/components/note-right/CreateNote.jsx
@@ -4,6 +4,7 @@ import CreateNoteEditor from "./CreateNoteEditor";
 import Container from "/src/ui/Container";
 import Group from "/src/ui/Group";
 import Input from "/src/ui/Input";
+import Box from "/src/ui/Box";
 
 export default function CreateNote() {
   return (
@@ -21,14 +22,21 @@ export default function CreateNote() {
           //       ? (e) => setCurrentNote({ ...currentNote, title: e.target.value })
           //       : onTitleChange
           //   }
+
+          //   dark:text-white dark:placeholder:text-gray-500
           classname={
-            "w-full border-b border-stone-300 dark:border-slate-700 bg-transparent pb-2 text-xl font-bold text-slate-900 placeholder:text-gray-400 focus:outline-none medium:text-2xl dark:text-white dark:placeholder:text-gray-500"
+            "w-full border-b borderStyling bg-transparent pb-2 text-xl font-bold text-slate-900 placeholder:text-gray-400 focus:outline-none medium:text-2xl"
           }
           //   disabled={disabled}
         />
-        <div className="mt-4 h-full flex-grow text-gray-900 placeholder:text-gray-500 dark:text-white dark:placeholder:text-gray-400">
+        <Box
+          adjustWidth={true}
+          classname={
+            "mt-4 h-full flex-grow text-gray-900 placeholder:text-gray-500 dark:text-white dark:placeholder:text-gray-400"
+          }
+        >
           {/* <EditorContent editor={editor} /> */}
-        </div>
+        </Box>
       </Group>
     </Container>
   );
