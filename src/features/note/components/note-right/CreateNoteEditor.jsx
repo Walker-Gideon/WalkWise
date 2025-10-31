fd677fb83ced95c4e59d56acec68b2bc7f4ac2b8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-31 17:26:46 +0000

    Set the layout for the CreateNoteEditor component

--- src/features/note/components/note-right/CreateNoteEditor.jsx
+++ src/features/note/components/note-right/CreateNoteEditor.jsx
@@ -1,3 +1,106 @@
-export default function CreateNoteEditor() {
-  return <div>CreateNoteEditor</div>;
+import {
+  LuAlignLeft,
+  LuAlignJustify,
+  LuAlignRight,
+  LuAlignCenter,
+} from "react-icons/lu";
+
+const editingTools = [
+  {
+    text: "H1",
+    style: "px-2.5",
+    /*
+    activeCheck: (editor) => editor.isActive("heading", { level: 1 }),
+    command: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 1 }).run(),*/
+  },
+  {
+    text: "H2",
+    /*
+    activeCheck: (editor) => editor.isActive("heading", { level: 2 }),
+    command: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 2 }).run(),*/
+  },
+  {
+    text: "B",
+    style: "font-bold px-[13px]",
+    /*
+    activeCheck: (editor) => editor.isActive("bold"),
+    command: (editor) => editor.chain().focus().toggleBold().run(),*/
+  },
+  {
+    text: "I",
+    style: "italic px-[14px]",
+    /*
+    activeCheck: (editor) => editor.isActive("italic"),
+    command: (editor) => editor.chain().focus().toggleItalic().run(),*/
+  },
+  {
+    text: "U",
+    style: "underline px-[11px]",
+    /*
+    activeCheck: (editor) => editor.isActive("underline"),
+    command: (editor) => editor.chain().focus().toggleUnderline().run(),*/
+  },
+  {
+    text: "H",
+    style: "px-[11px]",
+    /*
+    activeCheck: (editor) => editor.isActive("highlight"),
+    command: (editor) => editor.chain().focus().toggleHighlight().run(),*/
+  },
+];
+
+const alignments = [
+  { align: "left", icon: LuAlignLeft },
+  { align: "center", icon: LuAlignCenter },
+  { align: "right", icon: LuAlignRight },
+  { align: "justify", icon: LuAlignJustify },
+];
+
+export default function CreateNoteEditor({ editor }) {
+  const styling = {
+    base: "font-medium text-sm border p-2 rounded-sm cursor-pointer border-stone-300 dark:border-slate-700 transition-all duration-200",
+    isActive: "bg-slate-500 text-white hover:bg-slate-600",
+    notActive:
+      "text-slate-900 hover:text-white dark:text-white hover:bg-slate-600",
+  };
+
+  return (
+    <div
+      className={`mx-4 flex h-16 items-center gap-2 border-b border-stone-300 dark:border-slate-700`}
+    >
+      {editingTools.map((data, index) => (
+        <Button
+          key={index}
+          variant="outline"
+          classname={`${data.style} ${styling.base} ${data.activeCheck(editor) ? `${styling.isActive}` : `${styling.notActive}`}`}
+          onClick={(e) => {
+            e.preventDefault();
+            data.command(editor);
+          }}
+        >
+          {data.text}
+        </Button>
+      ))}
+
+      {alignments.map((btn, index) => (
+        <Button
+          key={index}
+          variant="outline"
+          classname={`px-[9.5px] py-[11px] ${styling.base} ${
+            editor.isActive({ textAlign: btn.align })
+              ? `${styling.isActive}`
+              : `${styling.notActive}`
+          }`}
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().setTextAlign(btn.align).run();
+          }}
+        >
+          <btn.icon />
+        </Button>
+      ))}
+    </div>
+  );
 }
