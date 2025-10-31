15a6d987382000f8b9343bf9877e12690952f63c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-31 17:39:12 +0000

    Update the CreateNote component

--- src/features/note/components/note-right/CreateNote.jsx
+++ src/features/note/components/note-right/CreateNote.jsx
@@ -1,11 +1,35 @@
+// import { EditorContent } from "@tiptap/react"; /*Install this dependence */
+
 import CreateNoteEditor from "./CreateNoteEditor";
 import Container from "/src/ui/Container";
+import Group from "/src/ui/Group";
+import Input from "/src/ui/Input";
 
 export default function CreateNote() {
   return (
     <Container adjust={true} classname={""}>
       <CreateNoteEditor />
-      CreateNote
+      <Group classname={"mx-4 my-2 h-full py-2"}>
+        <Input
+          id="note-title"
+          type="text"
+          name="title"
+          placeholder="Title"
+          //   value={readAlredyNote ? currentNote.title : title}
+          //   onChange={
+          //     readAlredyNote
+          //       ? (e) => setCurrentNote({ ...currentNote, title: e.target.value })
+          //       : onTitleChange
+          //   }
+          classname={
+            "w-full border-b border-stone-300 dark:border-slate-700 bg-transparent pb-2 text-xl font-bold text-slate-900 placeholder:text-gray-400 focus:outline-none medium:text-2xl dark:text-white dark:placeholder:text-gray-500"
+          }
+          //   disabled={disabled}
+        />
+        <div className="mt-4 h-full flex-grow text-gray-900 placeholder:text-gray-500 dark:text-white dark:placeholder:text-gray-400">
+          {/* <EditorContent editor={editor} /> */}
+        </div>
+      </Group>
     </Container>
   );
 }
