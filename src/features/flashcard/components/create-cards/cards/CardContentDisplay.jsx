c10047889d0a06358a84a398c3a754005147902a
Walker-Gideon <gideonawortwe3@gmail.com>
2025-10-31 19:32:48 +0000

    Set the layout for the card display

--- src/features/flashcard/components/create-cards/cards/CardContentDisplay.jsx
+++ src/features/flashcard/components/create-cards/cards/CardContentDisplay.jsx
@@ -1,3 +1,26 @@
+import Container from "/src/ui/Container";
+import Card from "/src/components/Card";
+
 export default function CardContentDisplay() {
-  return <div>CardContentDisplay</div>;
+  return (
+    <Container adjust={true} classname={"px-6 grid grid-cols-4 gap-6"}>
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+    </Container>
+  );
+}
+
+function Cards() {
+  return (
+    <Card classname={"cursor-pointer"}>
+      <h1 className="mb-6">Titile</h1>
+      <div className="flex items-center justify-between">
+        <p># card(s)</p>
+        <p>timing</p>
+      </div>
+    </Card>
+  );
 }
