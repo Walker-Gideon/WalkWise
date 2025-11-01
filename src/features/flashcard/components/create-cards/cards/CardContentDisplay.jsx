40ad3effef52586f1166d08db7f1d759a9fac21c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-01 22:16:55 +0000

    Adjustment to the Note over all secton

--- src/features/flashcard/components/create-cards/cards/CardContentDisplay.jsx
+++ src/features/flashcard/components/create-cards/cards/CardContentDisplay.jsx
@@ -1,5 +1,12 @@
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { GoDotFill } from "react-icons/go";
+import { LuPlay } from "react-icons/lu";
+
+import HeaderText from "/src/ui/HeaderText";
+import Paragraph from "/src/ui/Paragraph";
 import Container from "/src/ui/Container";
 import Card from "/src/components/Card";
+import Group from "/src/ui/Group";
 
 export default function CardContentDisplay() {
   return (
@@ -14,13 +21,24 @@ export default function CardContentDisplay() {
 }
 
 function Cards() {
+  const styling = {
+    button:
+      "rounded-sm bg-slate-500 p-2 text-white opacity-0 transition-colors group-hover:opacity-100 hover:bg-slate-600",
+    icon: "h-4 w-4",
+  };
+
   return (
-    <Card classname={"cursor-pointer"}>
-      <h1 className="mb-6">Titile</h1>
-      <div className="flex items-center justify-between">
-        <p># card(s)</p>
-        <p>timing</p>
-      </div>
+    <Card classname={"cursor-pointer group"}>
+      <HeaderText classname={"mb-6"}>Titile</HeaderText>
+      <Group classname={"flex items-center justify-between"}>
+        <Paragraph type="xs" classname={"text-slate-500 dark:text-slate-400"}>
+          # card(s)
+        </Paragraph>
+        <Paragraph>
+          timing{" "}
+          <GoDotFill className="h-3 w-3 text-slate-500 dark:text-slate-400" />
+        </Paragraph>
+      </Group>
     </Card>
   );
 }
