2e1e915272d07db271c1a4ffa1b2f17be29ea0a0
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-01 23:04:36 +0000

    Created the ScheduleContentLayouy

---
+++ src/features/schedule/components/ScheduleContentLayout.jsx
@@ -0,0 +1,12 @@
+export default function ScheduleContentLayout() {
+  return (
+    <div>
+      ScheduleContentLayout
+      {/* Display
+    1. Schedule Today
+    2. Schedule for the week
+    3. Schedule for the month
+     */}
+    </div>
+  );
+}
