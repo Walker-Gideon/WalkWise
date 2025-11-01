5461ea6ef522a50f85e35b6d958cb200b3f03fb8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-01 22:31:06 +0000

    Set the layout for the FormRow

--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import Container from "/src/ui/Container";
 
-export default function FormRow() {
-  return <div></div>;
+export default function FormRow({
+  children,
+  label,
+  error,
+  labStyling,
+  errStyling,
+}) {
+  return (
+    <Container>
+      {label && (
+        <label htmlFor={children.props.id} className={`${labStyling}`}>
+          {label}
+        </label>
+      )}
+      {children}
+      {error && <span className={`${errStyling}`}>{error}</span>}
+    </Container>
+  );
 }
