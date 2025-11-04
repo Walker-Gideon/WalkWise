ac9fcc79510eeea5737239c2b568c9752db935dd
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-04 22:58:40 +0000

    Created the ScheduleContentLayout componentds

--- src/features/schedule/components/ScheduleContentLayout.jsx
+++ src/features/schedule/components/ScheduleContentLayout.jsx
@@ -10,3 +10,8 @@ export default function ScheduleContentLayout() {
     </div>
   );
 }
+{
+  /* <HeaderText type="secondary" classname="mb-4">
+        Quick Actions
+      </HeaderText> */
+}
---
+++ src/features/schedule/components/ScheduleToday.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ScheduleToday() {
+  return <div>ScheduleToday</div>;
+}
