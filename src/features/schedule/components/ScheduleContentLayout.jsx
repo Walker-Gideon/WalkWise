96540b26b796194171a5391f7b85201f298e37a8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-04 23:05:16 +0000

    Adjustment in the schedule over all section

--- src/features/schedule/components/ScheduleContentLayout.jsx
+++ src/features/schedule/components/ScheduleContentLayout.jsx
@@ -1,13 +1,15 @@
+import Card from "/src/components/Card";
+
 export default function ScheduleContentLayout() {
   return (
-    <div className="relative space-y-6 lg:col-span-2">
+    <Card classname={"relative space-y-6 lg:col-span-2"}>
       ScheduleContentLayout
       {/* Display
     1. Schedule Today
     2. Schedule for the week
     3. Schedule for the month
      */}
-    </div>
+    </Card>
   );
 }
 {
