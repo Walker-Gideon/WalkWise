e7412545c08e9915959be5bee78b509ac158f25a
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-04 22:43:03 +0000

    Adjustment in the Schedule section

--- src/features/schedule/Schedules.jsx
+++ src/features/schedule/Schedules.jsx
@@ -16,11 +16,7 @@ export default function Schedules() {
         classname={"h-[510px] space-y-6 overflow-scroll p-6"}
       >
         <ScheduleStatus />
-        <Group
-          classname={
-            "grid grid-cols-1 gap-x-6 gap-y-3 lg:grid-cols-3 bg-amber-400"
-          }
-        >
+        <Group classname={"grid grid-cols-1 gap-x-6 gap-y-3 lg:grid-cols-3"}>
           <ScheduleContentLayout />
           <Group>
             <ScheduleActions />
