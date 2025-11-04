330dc07176eefdf69472b11264bca37984620b8a
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-04 22:41:04 +0000

    Set the layout for the ScheduleActions

--- src/features/schedule/components/ScheduleActions.jsx
+++ src/features/schedule/components/ScheduleActions.jsx
@@ -1,6 +1,9 @@
 import { LuPlus, LuPlay, LuChartColumnIncreasing } from "react-icons/lu";
 
-import Container from "/src/ui/Container";
+import HeaderText from "/src/ui/HeaderText";
+import Card from "/src/components/Card";
+import Button from "/src/ui/Button";
+import Group from "/src/ui/Group";
 
 const buttons = [
   {
@@ -13,17 +16,38 @@ const buttons = [
     icon: LuPlus,
     // to:
   },
-  {
-    text: "View Detailed Analytics",
-    icon: LuChartColumnIncreasing,
-    // to: "/dashboard",
-  },
 ];
 
 export default function ScheduleActions() {
+  const styling = "h-5 w-5 text-white";
+
   return (
-    <Container adjust={true} classname={""}>
-      ScheduleActions
-    </Container>
+    <Card classname={""}>
+      <HeaderText type="secondary" classname="mb-4">
+        Quick Actions
+      </HeaderText>
+
+      <Group>
+        {buttons.map((data, index) => (
+          <Button
+            key={index}
+            type="colors"
+            classname={`flex gap-3 w-full py-3 ${index === 1 ? "my-2" : ""}`}
+          >
+            {<data.icon className={styling} />}
+            {data.text}
+          </Button>
+        ))}
+
+        <Button
+          type="colors"
+          to="/dashboard"
+          classname={"flex gap-3 w-full py-3"}
+        >
+          {<LuChartColumnIncreasing className={styling} />}
+          View Detailed Analytics
+        </Button>
+      </Group>
+    </Card>
   );
 }
