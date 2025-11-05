28fd506ff929a0bd90c3f368dc08d97c6d06e05c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-05 11:30:30 +0000

    Update the Main component with the SchedulContext

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import App from "./App.jsx";
 
 import { FlashcardProvider } from "./features/flashcard/context/FlashcardContext.jsx";
 import { NavigationProvider } from "./contexts/NavigationContext.jsx";
+import { ScheduleProvider } from "./features/schedule/context/ScheduleContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <FlashcardProvider>
-      <NavigationProvider>
-        <App />
-      </NavigationProvider>
+      <ScheduleProvider>
+        <NavigationProvider>
+          <App />
+        </NavigationProvider>
+      </ScheduleProvider>
     </FlashcardProvider>
   </StrictMode>,
 );
