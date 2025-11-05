32999c3b2843db72edca498d1d8e314ccb34d7d6
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-05 11:18:26 +0000

    Update the SchedulHeader component with the Buttons

--- src/features/schedule/components/ScheduleHeader.jsx
+++ src/features/schedule/components/ScheduleHeader.jsx
@@ -1,10 +1,26 @@
 import Heading from "/src/components/Heading";
+import Button from "/src/ui/Button";
+
+const scheduleDate = ["Today", "Week", "Month"];
 
 export default function ScheduleHeader() {
   return (
     <Heading
       headerText="Study Schedule"
       paragraphText="Plan and track your learning sessions"
-    ></Heading>
+      // dark:bg-slate-800
+      groupStyling={"rounded-sm border borderStyling bg-white p-1 shadow-sm"}
+    >
+      {scheduleDate.map((view, index) => (
+        <Button
+          key={index}
+          // variant="secondary"
+          onclick={() => {}}
+          classname={""}
+        >
+          {view}
+        </Button>
+      ))}
+    </Heading>
   );
 }
