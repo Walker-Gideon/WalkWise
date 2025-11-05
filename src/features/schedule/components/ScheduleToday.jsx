b9a17d3bb57bb1252c143b453b65f7ba552588bd
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-05 12:23:01 +0000

    Update the ScheduleToday component

--- src/features/schedule/components/ScheduleToday.jsx
+++ src/features/schedule/components/ScheduleToday.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { LuCheck, LuPlay, LuPlus, LuClock, LuZap } from "react-icons/lu";
+
+import HeaderText from "/src/ui/HeaderText";
+import Button from "/src/ui/Button";
+import Group from "/src/ui/Group";
+import Flex from "/src/ui/Flex";
 
 export default function ScheduleToday() {
-  return <>ScheduleToday</>;
-}
-
-{
-  /* <HeaderText type="secondary" classname="mb-4">
-        Quick Actions
-      </HeaderText> */
+  return (
+    <>
+      <Flex variant="between">
+        <HeaderText type="secondary">Today's Sessions</HeaderText>
+        <Button
+          type="colors"
+          onclick={() => {}}
+          classname={"flex items-center gap-1"}
+        >
+          <LuPlus className="h-4 w-4" />
+          Add Session
+        </Button>
+      </Flex>
+      <Group>Today</Group>
+    </>
+  );
 }
