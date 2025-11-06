b813fb41a8ac102221f97e77798f4a6f45e39b9e
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-06 22:19:25 +0000

    Update in the Model component

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,12 @@
 import { LuX } from "react-icons/lu";
+import { LuTarget } from "react-icons/lu";
 
+import FormRow from "/src/components/FormRow";
 import HeaderText from "/src/ui/HeaderText";
 import Button from "/src/ui/Button";
+import Group from "/src/ui/Group";
+import Input from "/src/ui/Input";
+import Form from "/src/ui/Form";
 import Flex from "/src/ui/Flex";
 import Overlay from "./Overlay";
 
@@ -10,24 +15,43 @@ export default function Model() {
 
   // Default is Schedule
   return (
-    <Overlay
-      classname={
-        "w-full max-w-md rounded-2xl border border-slate-200 bg-white p-6 shadow-2xl dark:border-slate-700 dark:bg-slate-800"
-      }
-    >
-      <Flex variant="between">
-        <HeaderText type="secondary">Add Study Session</HeaderText>
-        <Button variant="secondary" onclick={() => {}} classname={""}>
-          <LuX />
-        </Button>
-      </Flex>
+    <Overlay>
+      <Group
+        //  dark:bg-slate-800
+        classname={
+          "w-full max-w-md rounded-2xl border borderStyling bg-white p-6 shadow-2xl "
+        }
+      >
+        <Flex variant="between">
+          <HeaderText type="secondary">Add Study Session</HeaderText>
+          <Button variant="secondary" onclick={() => {}} classname={""}>
+            <LuX />
+          </Button>
+        </Flex>
 
-      {/* Form */}
-      {/* 
-      Select tag input
+        {/* Form */}
+        {/* 
+      
     Auto generated number of cards
     (date and time) 
     Cancel and add session buttons  */}
+        <Form onsubmit={() => {}} classname={""}>
+          {/* Select tag input */}
+
+          <FormRow label="Number of Cards">
+            <Group classname={"relative+"}>
+              <LuTarget className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
+              <Input
+                type="number"
+                placeholder="Auto-filled after selecting tag"
+                classname={`w-full dark:text-white pl-10  disabled:cursor-not-allowed`}
+                disabled={true}
+                //   value={formData.count}
+              />
+            </Group>
+          </FormRow>
+        </Form>
+      </Group>
     </Overlay>
   );
 }
