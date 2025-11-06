70704719992bf0f8d23b3e38071351f0c46a2499
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-06 11:59:37 +0000

    Set the layout for the Overlay Component

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import Group from "/src/ui/Group";
 
-export default function Overlay() {
-  return <div></div>;
+export default function Overlay({ classname, children }) {
+  return (
+    <Group
+      classname={`absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm z-50 ${classname}`}
+    >
+      {children}
+    </Group>
+  );
 }
