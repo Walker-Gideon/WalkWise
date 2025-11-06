9ddbf32d699e53ad342674c32f923d189a98de3c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-06 12:04:49 +0000

    Update the Overlay Component

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,11 +1,19 @@
 import Group from "/src/ui/Group";
 
-export default function Overlay({ classname, children }) {
-  return (
-    <Group
-      classname={`absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm z-50 ${classname}`}
-    >
-      {children}
-    </Group>
-  );
+export default function Overlay({ children, classname, button, onclick }) {
+  const styling =
+    "absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm z-50";
+
+  if (button)
+    return (
+      <div
+        role="button"
+        onClick={onclick}
+        className={`${styling} ${classname}`}
+      >
+        {children}
+      </div>
+    );
+
+  return <Group classname={`${styling} ${classname}`}>{children}</Group>;
 }
