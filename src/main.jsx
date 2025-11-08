ba6d414c4514be6cc4db0447d02f6651df77d443
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-08 04:53:51 +0000

    Update the Main with the Context

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,17 @@ import App from "./App.jsx";
 import { FlashcardProvider } from "./features/flashcard/context/FlashcardContext.jsx";
 import { NavigationProvider } from "./contexts/NavigationContext.jsx";
 import { ScheduleProvider } from "./features/schedule/context/ScheduleContext.jsx";
+import { InspireProvider } from "./features/inspire/context/InspireContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <FlashcardProvider>
       <ScheduleProvider>
-        <NavigationProvider>
-          <App />
-        </NavigationProvider>
+        <InspireProvider>
+          <NavigationProvider>
+            <App />
+          </NavigationProvider>
+        </InspireProvider>
       </ScheduleProvider>
     </FlashcardProvider>
   </StrictMode>,
