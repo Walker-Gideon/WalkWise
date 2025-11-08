9f6a4aec7d965e262cd5a5f4da71d242f3ac0f37
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-08 05:04:17 +0000

    Update in the Ispire section

--- src/features/inspire/Inspire.jsx
+++ src/features/inspire/Inspire.jsx
@@ -1,11 +1,24 @@
 import InspireHeader from "./components/InspireHeader";
 import Container from "/src/ui/Container";
+import Group from "/src/ui/Group";
+import Main from "/src/ui/Main";
 
 export default function Inspire() {
   return (
     <Container>
       <InspireHeader />
-      <h1>Inspire</h1>
+      <Main classname={"grid grid-cols-1 gap-x-6 gap-y-3 lg:grid-cols-3"}>
+        <Group classname={"space-y-6 lg:col-span-2"}>
+          {/* Motivation
+          Achievement
+          Progress */}
+        </Group>
+        <Group classname={"space-y-6"}>
+          {/* StreakCounter
+          praise
+          ActionInspire */}
+        </Group>
+      </Main>
     </Container>
   );
 }
