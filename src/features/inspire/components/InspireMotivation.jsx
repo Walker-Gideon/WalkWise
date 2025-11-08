c376058ca1c14551042ece2c6d5fb91bba2756d0
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-08 05:53:38 +0000

    Created the components for the left side of the Inspire grid

---
+++ src/features/inspire/components/InspireMotivation.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function InspireMotivation() {
+  return <div>InspireMotivation</div>;
+}
---
+++ src/features/inspire/components/InspireProgress.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function InspireProgress() {
+  return <div>InspireProgress</div>;
+}
