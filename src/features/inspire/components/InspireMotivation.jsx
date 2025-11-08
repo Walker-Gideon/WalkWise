27b6a21527dda9f694a0ad53ee00b4a8a0335788
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-08 06:04:37 +0000

    Update in the InspireMotivation

--- src/features/inspire/components/InspireMotivation.jsx
+++ src/features/inspire/components/InspireMotivation.jsx
@@ -1,30 +1,36 @@
 import { LuLightbulb } from "react-icons/lu";
 
 import HeaderText from "/src/ui/HeaderText";
+import Paragraph from "/src/ui/Paragraph";
 import SpanText from "/src/ui/SpanText";
-import Group from "/src/ui/Group";
 import Card from "/src/components/Card";
+import Group from "/src/ui/Group";
 
 export default function InspireMotivation() {
   return (
     <Card>
       <SpanText classname={"mb-4 flex items-center space-x-3"}>
         <LuLightbulb className="h-6 w-6 text-slate-600 dark:text-slate-300" />
-        <HeaderText>Daily Inspiration</HeaderText>
+        <HeaderText variant="secondary">Daily Inspiration</HeaderText>
       </SpanText>
       <Group>
-        {currentQuote ? (
-          <>
-            <blockquote className="mb-4 text-lg text-slate-500 italic dark:text-slate-400">
-              "{currentQuote.text}"
-            </blockquote>
-            <p className="text-right text-sm">— {currentQuote.author}</p>
-          </>
-        ) : (
-          <>
-            <div className="italic dark:text-slate-400">Loading quote...</div>
-          </>
-        )}
+        {/* {currentQuote ? ( */}
+        <>
+          <blockquote className="mb-4 text-lg text-slate-500 italic dark:text-slate-400">
+            {/* "{currentQuote.text}" */}"Learning is a treasure that follows
+            its owner everywhere."
+          </blockquote>
+          <Paragraph classname={"text-right text-sm"}>
+            {/* — {currentQuote.author} */} Auther Name
+          </Paragraph>
+        </>
+        {/* ) : ( */}
+        <>
+          {/* <Paragraph classname={"italic dark:text-slate-400"}>
+            Loading quote...
+          </Paragraph> */}
+        </>
+        {/* )} */}
       </Group>
     </Card>
   );
