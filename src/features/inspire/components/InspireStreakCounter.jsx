9674e90102250c9fb67c7b1136353fa19f94a3e8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-08 05:14:51 +0000

    Set the layout for InspireStreakCounter

--- src/features/inspire/components/InspireStreakCounter.jsx
+++ src/features/inspire/components/InspireStreakCounter.jsx
@@ -1,5 +1,22 @@
+import { LuFlame } from "react-icons/lu";
+
 import Card from "/src/components/Card";
 
 export default function InspireStreakCounter() {
-  return <Card>InspireStreakCounter</Card>;
+  return (
+    <Card>
+      <LuFlame className="mx-auto mb-3 h-12 w-12 text-slate-600 dark:text-slate-300" />
+      <p className="text-4xl font-bold text-slate-900 dark:text-white">
+        {userData.streakCount}
+      </p>
+      <p className="mb-3 text-lg text-slate-600 dark:text-slate-300">
+        Day Streak!
+      </p>
+      {userData.streakCount !== 0 && (
+        <p className="text-sm text-slate-500 dark:text-slate-400">
+          Don’t break your streak!
+        </p>
+      )}
+    </Card>
+  );
 }
