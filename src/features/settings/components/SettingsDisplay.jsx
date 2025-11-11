a3fe109047c8e9b7441df44a195ce326f29b20af
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-11 21:03:46 +0000

    Update the SettingsDisplay

--- src/features/settings/components/SettingsDisplay.jsx
+++ src/features/settings/components/SettingsDisplay.jsx
@@ -1,3 +1,22 @@
+import Card from "/src/components/Card";
+import Paragraph from "/src/ui/Paragraph";
+import HeaderText from "/src/ui/HeaderText";
+import Header from "/src/ui/Header";
+import Group from "/src/ui/Group";
+
 export default function SettingsRight() {
-  return <div>SettingsRight</div>;
+  return (
+    <Card classname={"mt-6 w-[50%] ml-5"}>
+      <Header classname={"mb-4"}>
+        <HeaderText variant="secondary">Profile Information</HeaderText>
+        <Paragraph variant="small" classname={"dark:text-slate-400"}>
+          Update your profile information visible to other users
+        </Paragraph>
+      </Header>
+
+      <Group>
+        <Group></Group>
+      </Group>
+    </Card>
+  );
 }
