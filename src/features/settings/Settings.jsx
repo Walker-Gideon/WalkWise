d6e71aeacbd60b277f95ada0ee2b731cf4a00d75
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-11 20:29:10 +0000

    Created Settings sub components

--- src/features/settings/Settings.jsx
+++ src/features/settings/Settings.jsx
@@ -2,6 +2,9 @@ export default function Settings() {
   return (
     <div>
       <h1>Settings</h1>
+      {/* Header */}
+
+      {/* Main (with grid for left and right side) */}
     </div>
   );
 }
---
+++ src/features/settings/components/SettingsRight.jsx
@@ -0,0 +1,3 @@
+export default function SettingsRight() {
+  return <div>SettingsRight</div>;
+}
