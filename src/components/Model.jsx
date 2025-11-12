384a106834b2a7ce7d975806604f260a088399bd
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-12 12:08:34 +0000

    Update the Model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,5 +1,7 @@
 import { LuX } from "react-icons/lu";
 import { LuTarget } from "react-icons/lu";
+import { LuCalendar } from "react-icons/lu";
+import { LuClock } from "react-icons/lu";
 
 import FormRow from "/src/components/FormRow";
 import HeaderText from "/src/ui/HeaderText";
@@ -39,21 +41,50 @@ export default function Model({ onDisplaySessionForm }) {
     Auto generated number of cards
     (date and time) 
     Cancel and add session buttons  */}
-        <Form onsubmit={() => {}} classname={""}>
+        <Form onsubmit={() => {}} classname={"mt-6"}>
           {/* Select tag input */}
 
+          {/* Number of Cards */}
           <FormRow label="Number of Cards">
-            <Group classname={"relative+"}>
+            <Group classname={"relative"}>
               <LuTarget className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
               <Input
                 type="number"
                 placeholder="Auto-filled after selecting tag"
-                classname={`w-full dark:text-white pl-10  disabled:cursor-not-allowed`}
+                classname={`w-full dark:text-white pl-8  disabled:cursor-not-allowed`}
                 disabled={true}
                 //   value={formData.count}
               />
             </Group>
           </FormRow>
+
+          <Flex variant="between" classname={"gap-2 my-4"}>
+            <FormRow label="Date *">
+              <Group classname={"relative"}>
+                <LuCalendar className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
+                <Input
+                  type="date"
+                  classname={`w-full pl-8`} //dark:text-white
+                  //   value={}
+                  // onChange={}
+                  // disabled={}
+                />
+              </Group>
+            </FormRow>
+
+            <FormRow label="Time *">
+              <Group classname={"relative"}>
+                <LuClock className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
+                <Input
+                  type="time"
+                  classname={`w-full pl-8`} //dark:text-white
+                  //   value={}
+                  // onChange={}
+                  // disabled={}
+                />
+              </Group>
+            </FormRow>
+          </Flex>
         </Form>
       </Group>
     </Overlay>
