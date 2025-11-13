1d971b2e0cce718a2def63fbdd91f7f2b932cd9a
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-13 03:44:22 +0000

    Set up react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import LandingPage from "./landingPage/LandingPage";
 import SignIn from "./authentication/sign-in/SignIn";
 import SignUp from "./authentication/sign-up/SignUp";
@@ -14,32 +16,41 @@ import Settings from "./features/settings/Settings";
 import Verify from "./authentication/verify/Verify";
 import PageNotFound from "./ui/PageNotFound";
 
+const queryClient = new QueryClient();
+// defaultOptions: {
+//   queries: {
+//     staleTime: 60 * 1000,
+//   },
+// },
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<LandingPage />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage />} />
 
-        <Route element={<Accounts />}>
-          <Route index element={<Navigate replace to="sign-in" />} />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="forgotten" element={<ForgetAccount />} />
-          <Route path="verify" element={<Verify />} />
-        </Route>
+          <Route element={<Accounts />}>
+            <Route index element={<Navigate replace to="sign-in" />} />
+            <Route path="sign-in" element={<SignIn />} />
+            <Route path="sign-up" element={<SignUp />} />
+            <Route path="forgotten" element={<ForgetAccount />} />
+            <Route path="verify" element={<Verify />} />
+          </Route>
 
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="notes" element={<Note />} />
-          <Route path="flashcards" element={<Flashcard />} />
-          <Route path="schedule" element={<Schedules />} />
-          <Route path="inspire" element={<Inspire />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="notes" element={<Note />} />
+            <Route path="flashcards" element={<Flashcard />} />
+            <Route path="schedule" element={<Schedules />} />
+            <Route path="inspire" element={<Inspire />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
 
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
