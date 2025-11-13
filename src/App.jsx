893bd1959f0f30a3220de2ed6b17584ce1052780
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-13 03:51:09 +0000

    Set the Toaster api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import LandingPage from "./landingPage/LandingPage";
-import SignIn from "./authentication/sign-in/SignIn";
-import SignUp from "./authentication/sign-up/SignUp";
 import ForgetAccount from "./authentication/forget/ForgetAccount";
-import Accounts from "./authentication/Accounts";
-import AppLayout from "./ui/AppLayout";
 import Dashboard from "./features/dashboard/Dashboard";
-import Note from "./features/note/Note";
 import Flashcard from "./features/flashcard/Flashcard";
 import Schedules from "./features/schedule/Schedules";
-import Inspire from "./features/inspire/Inspire";
+import SignIn from "./authentication/sign-in/SignIn";
+import SignUp from "./authentication/sign-up/SignUp";
 import Settings from "./features/settings/Settings";
+import LandingPage from "./landingPage/LandingPage";
 import Verify from "./authentication/verify/Verify";
+import Accounts from "./authentication/Accounts";
+import Inspire from "./features/inspire/Inspire";
 import PageNotFound from "./ui/PageNotFound";
+import Note from "./features/note/Note";
+import AppLayout from "./ui/AppLayout";
 
 const queryClient = new QueryClient();
 // defaultOptions: {
@@ -51,6 +52,28 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        reverseOrder={false}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "14px",
+            maxWidth: "500px",
+            padding: "14px 24px",
+            background: "#fff",
+            color: "#374151",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
