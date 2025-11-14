ef5b7d74993edaa2767cac1d8a73b8f147326090
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-14 13:37:06 +0000

    Update in the DashboardMotivation

--- src/features/dashboard/components/DashboardMotivation.jsx
+++ src/features/dashboard/components/DashboardMotivation.jsx
@@ -1,4 +1,5 @@
 import { LuLightbulb, LuFlame } from "react-icons/lu";
+
 import HeaderText from "/src/ui/HeaderText";
 import SpanText from "/src/ui/SpanText";
 import Card from "/src/components/Card";
@@ -6,17 +7,11 @@ import Card from "/src/components/Card";
 export default function DashboardMotivation() {
   return (
     <Card>
-      <HeaderText
-        type="secondary"
-        classname={"mb-4 flex items-center space-x-2"}
-      >
-        <LuLightbulb
-          // dark:text-slate-300
-          className="h-5 w-5 text-slate-600"
-        />
+      <HeaderText type="secondary" classname={"mb-4 flex items-center gap-2"}>
+        <LuLightbulb className="icons mb-1" />
         <SpanText>Daily Inspiration</SpanText>
       </HeaderText>
-      <blockquote className="mb-4 text-sm text-slate-500 italic dark:text-slate-400">
+      <blockquote className="secondary-text-color mb-4 text-sm italic">
         {/* "{currentQuote ? currentQuote?.text : "Loading quote..."}" */}
         The mind is not a vessel to be filled, but a fire to be kindled.
       </blockquote>
