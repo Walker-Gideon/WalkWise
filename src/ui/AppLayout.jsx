d08cc0de6e1f9170cb681cc4068a30f4a8a3b191
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-14 13:49:09 +0000

    Update in the AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,22 +1,22 @@
 import { Outlet } from "react-router-dom";
 
-import { useNav } from "/src/contexts/NavigationContext";
 import Slider from "/src/navigation/Slider";
 import Container from "/src/ui/Container";
 import Main from "/src/ui/Main";
+
+import { useNav } from "/src/contexts/NavigationContext";
 
 export default function AppLayout() {
   const { isExpanded } = useNav();
 
   return (
     <Container
-      // grid grid-rows-[auto_1fr]  ${adjustNavigationWidth ? "grid-cols-[auto_1fr]" : "grid-cols-[14rem_1fr]"}
       classname={`defaultColor flex overflow-hidden ${isExpanded ? "" : ""}`}
     >
       <Slider />
 
       <Main
-        classname={"h-screen w-full bg-slate-50 dark:bg-slate-700 transition"}
+        classname={"h-screen w-full bg-slate-50 dark:bg-slate-800 transition"}
       >
         <Outlet />
       </Main>
