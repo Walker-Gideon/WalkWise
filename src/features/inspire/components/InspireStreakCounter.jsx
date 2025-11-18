d4ac3b64d758eb2e8eacdf9a794ac580f1982d01
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-18 18:43:10 +0000

    Update in the InspireStreakCounter

--- src/features/inspire/components/InspireStreakCounter.jsx
+++ src/features/inspire/components/InspireStreakCounter.jsx
@@ -11,16 +11,15 @@ export default function InspireStreakCounter() {
         <LuFlame className="mx-auto h-12 w-12 text-slate-600 dark:text-slate-300" />
       </SpanText>
       <Paragraph
-        //   dark:text-white
-        classname={"text-4xl font-bold text-slate-900 my-2"}
+        classname={"text-4xl font-bold primary-text-color my-2"}
       >
         {/* {userData.streakCount} */} X
       </Paragraph>
-      <Paragraph classname={"text-lg text-slate-600 dark:text-slate-300"}>
+      <Paragraph classname={"text-lg primary-text-color"}>
         Day Streak!
       </Paragraph>
       {/* {userData.streakCount !== 0 && ( */}
-      <Paragraph variant="small" classname={"dark:text-slate-400"}>
+      <Paragraph variant="small" classname={"secondary-text-color"}>
         Don’t break your streak!
       </Paragraph>
       {/* )} */}
