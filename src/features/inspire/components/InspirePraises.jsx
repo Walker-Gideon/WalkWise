a122ab57515ced05b556ea2cf567f8906ffae14d
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-18 18:07:08 +0000

    Adjustment in the InspirePraises

--- src/features/inspire/components/InspirePraises.jsx
+++ src/features/inspire/components/InspirePraises.jsx
@@ -1,7 +1,7 @@
 import { LuAward } from "react-icons/lu";
 
-import Paragraph from "/src/ui/Paragraph";
 import HeaderText from "/src/ui/HeaderText";
+import Paragraph from "/src/ui/Paragraph";
 import SpanText from "/src/ui/SpanText";
 import Card from "/src/components/Card";
 import Group from "/src/ui/Group";
@@ -11,17 +11,19 @@ export default function InspirePraises() {
     <Card>
       <SpanText classname={"mb-4 flex items-center space-x-3"}>
         <LuAward className="h-6 w-6 text-slate-600 dark:text-slate-300" />
-        <HeaderText variant="secondary">Your Encouragement</HeaderText>
+        <HeaderText type="secondary">Your Encouragement</HeaderText>
       </SpanText>
       <Group>
+        {/* Conditional here */}
         <Paragraph
           className={"mb-4 text-lg text-slate-500 italic dark:text-slate-400"}
         ></Paragraph>
-        <Paragraph classname={"italic dark:text-slate-400"}>
+        {/* Conditional here */}
+        <Paragraph classname={"italic text-slate-500 dark:text-slate-400"}>
           Loading praise...
         </Paragraph>
         <Paragraph
-          classname={"text-right text-sm text-slate-900 dark:text-white"}
+          classname={"text-right text-sm primary-text-color"}
         >
           - WalkWise AI
         </Paragraph>
