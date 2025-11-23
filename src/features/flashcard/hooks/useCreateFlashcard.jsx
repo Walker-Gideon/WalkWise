a7d908664fb2d694172d64ee9848affba8d9478e
Walker-Gideon <gideonawortwe3@gmail.com>
2025-11-23 20:42:06 +0000

    Adjustment in the useCreateFlashcard hook

--- src/features/flashcard/hooks/useCreateFlashcard.jsx
+++ src/features/flashcard/hooks/useCreateFlashcard.jsx
@@ -2,22 +2,27 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 import { createFlashcard as createFlashcardAPI } from "/src/service/apiFlashcard";
+import { useUserData } from "/src/user/hook/useUserData";
 
-export default function useCreateFlashcard(userId = null) {
+export default function useCreateFlashcard() {
   const queryClient = useQueryClient();
 
+  const { firebaseUser } = useUserData();
+  const userId = firebaseUser?.uid;
+
   const { mutate: createFlashcard, isPending: isCreating } = useMutation({
-    mutationFn: createFlashcardAPI,
+    mutationFn: (data) => {
+      if (!userId) {
+        toast.error("You must be logged in to create a flashcard.");
+      }
+
+      return createFlashcardAPI({ ...data, userId });
+    },
     onSuccess: () => {
-      toast.success("Flashcard successfully created");
+      toast.success("Flashcard created successfully");
       queryClient.invalidateQueries({ queryKey: ["flashcards", userId] });
-      console.log("Flashcard created!");
-      //   reset();
     },
-    onError: (err) => {
-      console.error(err);
-    },
-    // onError: (err) => toast.error(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return { createFlashcard, isCreating };
