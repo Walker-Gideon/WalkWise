63e8a253bf4c806b0a6054ca5f122f23d0541b85
Walker-Gideon <gideonawortwe3@gmail.com>
2025-12-08 18:59:47 +0000

    Created the StudyFlashcardHeader

--- src/features/flashcard/components/create-cards/StudyFlashcard.jsx
+++ src/features/flashcard/components/create-cards/StudyFlashcard.jsx
@@ -42,35 +42,7 @@ export default function StudyFlashcard() {
 
     return (
         <Container classname={"p-4"}>
-            <Header classname={"mb-4 py-6 middle:px-4 lg:px-8 flex items-center justify-between"}>
-                <Flex classname={"items-center gap-2"}>
-                    <Button
-                      variant="secondary" 
-                      type="back"
-                      onclick={handleBack}
-                    >
-                        <LuArrowLeft className="w-5 h-5" />
-                    </Button>
-                    <HeaderText classname={"text-xl font-bold text-slate-900 dark:text-white"}>{title}</HeaderText>
-                </Flex>
-
-                <Flex classname={"items-center justify-end"}>
-                    <Menus>
-                        <Menus.Toggle />
-
-                        <Menus.Lists>
-                            <Menus.Buttons onClick={handleEdit}>
-                                <RiEditLine className="w-4 h-4" />
-                                Edit
-                            </Menus.Buttons>
-                            <Menus.Buttons onClick={handleDelete}>
-                                <RiDeleteBin5Line className="w-4 h-4" />
-                                Delete
-                            </Menus.Buttons>
-                        </Menus.Lists>
-                    </Menus>
-                </Flex>
-            </Header>
+            {/*  */}
 
             {isPending ? (
                 <Spinner />
