f97de4931b500f798d19f5ef5109a63da0c770f2
Walker-Gideon <gideonawortwe3@gmail.com>
2025-12-09 23:31:01 +0000

    Working on the StudyFlashcardSummary section

--- src/features/flashcard/components/create-cards/StudyFlashcardSummary.jsx
+++ src/features/flashcard/components/create-cards/StudyFlashcardSummary.jsx
@@ -1,18 +1,33 @@
-import { LuCheck } from "react-icons/lu";
-import { LuArrowLeft } from "react-icons/lu";
-import { LuFlame } from "react-icons/lu";
+import { LuArrowLeft, LuFlame, LuCheck } from "react-icons/lu";
 
-import Button from "/src/ui/Button";
 import HeaderText from "/src/ui/HeaderText";
+import Container from "/src/ui/Container";
+import Header from "/src/ui/Header";
+import Button from "/src/ui/Button";
+import Flex from "/src/ui/Flex";
 
 export default function StudyFlashcardSummary() {
+    function handleBack() {}
   return (
-    <div className="mt-10 w-full p-6 md:mx-auto md:max-w-3xl">
-      <div className="flex w-full items-center justify-between">
+    // mt-10 w-full p-6 md:mx-auto md:max-w-3xl
+    <Container classname={"p-4"}>
+      <Header classname={"mb-4 py-6 middle:px-4 lg:px-8"}>
+        <Flex classname={"items-center gap-2"}>
+            <Button
+              variant="secondary" 
+              type="back"
+              onclick={handleBack}
+            >
+                <LuArrowLeft className="w-5 h-5" />
+            </Button>
+
+            <HeaderText classname={"text-xl font-bold text-slate-900 dark:text-white"}>
+               Review Complete!
+            </HeaderText>
+        </Flex>
+      </Header>
+
         <div className="">
-          <HeaderText className="text-2xl font-bold">
-            Review Complete!
-          </HeaderText>
           <p className="medium:text-4xl middle:text-5xl mt-2 text-2xl text-slate-900 lg:text-6xl dark:text-white">
             You reviewed all your flashcards.
           </p>
@@ -21,7 +36,6 @@ export default function StudyFlashcardSummary() {
         <span className="medium:text-9xl -rotate-90 transform text-7xl">
           🎉
         </span>
-      </div>
 
       <p className="medium:text-xl mt-6 mb-4 font-medium text-slate-900 dark:text-white">
         How you're doing
@@ -71,6 +85,6 @@ export default function StudyFlashcardSummary() {
           Back to Flashcard
         </Button>
       </div>
-    </div>
+    </Container>
   );
 }
