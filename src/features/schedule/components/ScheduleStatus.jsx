e1a13dec5e9d7dee8172da933272a452f6b77db1
Walker-Gideon <gideonawortwe3@gmail.com>
2025-12-24 16:35:42 +0000

    Update the ScheculeStatus

--- src/features/schedule/components/ScheduleStatus.jsx
+++ src/features/schedule/components/ScheduleStatus.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { LuTarget, LuClock, LuFlame, LuTrendingUp } from "react-icons/lu";
 
 import HeaderText from "/src/ui/HeaderText";
@@ -5,6 +6,8 @@ import Badge from "/src/components/Badge";
 import Paragraph from "/src/ui/Paragraph";
 import Card from "/src/components/Card";
 import Group from "/src/ui/Group";
+
+import { useUserData } from "/src/user/hook/useUserData";
 
 const status = [
   {
@@ -30,13 +33,70 @@ const status = [
 ];
 
 export default function ScheduleStatus() {
+  const { userData, loading, error } = useUserData();
+  // console.log(userData);
+
+  const [statusData, setStatusData] = useState(status)
+
+  useEffect(() => {
+    const updated = status.map((card) => {
+      if (card.text === "Cards Today") {
+        return { ...card, data: 1 }; // will change the data here
+      }
+
+      if (card.text === "Day Streak") {
+        return { ...card, data: userData?.streakCount };
+      }
+
+      return card; 
+    })
+
+    setStatusData(updated);
+  }, [])
+
+  /*
+  const { progress, loadingProgress, consistencyScore, todayFlashcards } =
+    useGen();
+  const [cardData, setCardData] = useState(initialCardData);
+
+  useEffect(() => {
+    if (!loadingProgress && progress) {
+      const today = new Date().toISOString().split("T")[0];
+      const todayCards = progress.studyLogs?.[today] || 0;
+
+      const updated = initialCardData.map((card) => {
+        if (card.text === "Cards Today") {
+          return { ...card, data: todayFlashcards?.length };
+        }
+
+        if (card.text === "Day Streak") {
+          return { ...card, data: progress?.streakCount };
+        }
+
+        if (card.text === "Study Time") {
+          return { ...card, data: todayCards + "m" };
+        }
+
+        if (card.text === "Success Rate") {
+          return { ...card, data: consistencyScore + "%" };
+        }
+
+        return card;
+      });
+
+      setCardData(updated);
+    }
+  }, [loadingProgress, progress, consistencyScore, todayFlashcards?.length]);
+
+  */ 
+
   return (
     <Group
       classname={
         "medium:grid-cols-2 grid grid-cols-1 gap-4 medium:gap-6 lg:grid-cols-4"
       }
     >
-      {status.map((stats, index) => (
+      {statusData.map((stats, index) => (
         <Card
           key={index}
           classname={"hover:shadow-lg flex items-center justify-between"}
