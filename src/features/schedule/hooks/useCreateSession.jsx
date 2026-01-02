7e2626691a6f7293473352f183377e2360b62554
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-02 14:51:44 +0000

    Update in the create session

---
+++ src/features/schedule/hooks/useCreateSession.jsx
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import { createSession as createSessionAPI } from "/src/service/apiSchedule";
+import { useUserData } from "/src/user/hook/useUserData";
+
+export default function useCreateSession() {
+    const queryClient = useQueryClient();
+
+    const { firebaseUser } = useUserData();
+    const userId = firebaseUser?.uid;
+
+    const { mutate: createSession, isPending: isCreating } = useMutation({
+        mutationFn: (data) => {
+            if (!userId) {
+                toast.error("You must be logged in to create a session.");
+            }
+
+            return createSessionAPI({ ...data, userId });
+        },
+        onSuccess: () => {
+            toast.success("Session created successfully");
+            queryClient.invalidateQueries({ queryKey: ["sessions", userId] });
+        },
+        onError: (err) => toast.error(err.message),
+    });
+
+    return { createSession, isCreating };
+}
