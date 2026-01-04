41cb4f3e281bf9e94fa778c4d4239920a9b673f7
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-04 20:48:39 +0000

    Added the schedule card to the schedule today

--- src/features/schedule/components/ScheduleToday.jsx
+++ src/features/schedule/components/ScheduleToday.jsx
@@ -1,8 +1,10 @@
 import { LuCheck, LuPlay, LuPlus, LuClock, LuZap } from "react-icons/lu";
+import { RiDeleteBin5Line, RiEditLine } from "react-icons/ri";
 
 // import Conditional from "/src/components/Conditional";
 import HeaderText from "/src/ui/HeaderText";
 import Paragraph from "/src/ui/Paragraph";
+import Menus from "/src/components/Menus";
 import Card from "/src/components/Card";
 import Button from "/src/ui/Button";
 import Group from "/src/ui/Group";
@@ -40,8 +42,8 @@ export default function ScheduleToday() {
       </Conditional> */}
       <Group classname={"h-190 space-y-3 overflow-y-scroll"}>
         <Card>
-          {/* Group */}
-          <Flex variant="center">
+          <Flex variant="between">
+            <Flex variant="center">
             <div
               className={`h-4 w-4 rounded-full bg-gradient-to-r from-slate-200 to-slate-300 dark:from-slate-600 dark:to-slate-700`}
             ></div>
@@ -54,8 +56,27 @@ export default function ScheduleToday() {
                </p>
             </div>
           </Flex>
+          <Group>
+            <Menus>
+              <Menus.Toggle />
 
-          {/* Group */} 
+              <Menus.Lists>
+                <Menus.Buttons onClick={() => {}}>
+                  <LuPlay className="w-4 h-4" />
+                  Play
+                </Menus.Buttons>
+                <Menus.Buttons onClick={() => {}}>
+                  <RiEditLine className="w-4 h-4" />
+                  Edit
+                </Menus.Buttons>
+                <Menus.Buttons onClick={() => {}}>
+                  <RiDeleteBin5Line className="w-4 h-4" />
+                  Delete
+                </Menus.Buttons>
+              </Menus.Lists>
+            </Menus>
+          </Group>
+          </Flex>
         </Card>
       </Group>
     </>
