78414337d2a5aa4d466d2a892400c85f5493a1b4
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-04 20:58:22 +0000

    Update the SessionCard and adjustment in the ScheduleToday

--- src/features/schedule/components/ScheduleToday.jsx
+++ src/features/schedule/components/ScheduleToday.jsx
@@ -1,13 +1,8 @@
 import { LuCheck, LuPlay, LuPlus, LuClock, LuZap } from "react-icons/lu";
-import { RiDeleteBin5Line, RiEditLine } from "react-icons/ri";
 
 // import Conditional from "/src/components/Conditional";
 import HeaderText from "/src/ui/HeaderText";
-import Paragraph from "/src/ui/Paragraph";
-import Menus from "/src/components/Menus";
-import Card from "/src/components/Card";
 import Button from "/src/ui/Button";
-import Group from "/src/ui/Group";
 import Flex from "/src/ui/Flex";
 
 import { useSchedule } from "../context/ScheduleContext";
@@ -40,43 +35,6 @@ export default function ScheduleToday() {
       </Group> */}
       {/* <Conditional condition={false}>
       </Conditional> */}
-      <Group classname={"h-190 space-y-3 overflow-y-scroll"}>
-        <Card>
-          <Flex variant="between">
-            <Flex variant="center">
-            <div
-              className={`h-4 w-4 rounded-full bg-gradient-to-r from-slate-200 to-slate-300 dark:from-slate-600 dark:to-slate-700`}
-            ></div>
-            <div>
-               <p className={"font-medium text-slate-900 dark:text-white"} >Schedule title</p>
-               <p className="text-sm text-slate-500 dark:text-slate-400">
-                schedule$Count cards • schedule$EstimatedTime min
-               </p>
-            </div>
-          </Flex>
-          <Group>
-            <Menus>
-              <Menus.Toggle />
-
-              <Menus.Lists>
-                <Menus.Buttons onClick={() => {}}>
-                  <LuPlay className="w-4 h-4" />
-                  Play
-                </Menus.Buttons>
-                <Menus.Buttons onClick={() => {}}>
-                  <RiEditLine className="w-4 h-4" />
-                  Edit
-                </Menus.Buttons>
-                <Menus.Buttons onClick={() => {}}>
-                  <RiDeleteBin5Line className="w-4 h-4" />
-                  Delete
-                </Menus.Buttons>
-              </Menus.Lists>
-            </Menus>
-          </Group>
-          </Flex>
-        </Card>
-      </Group>
     </>
   );
 }
--- src/features/schedule/components/SessionCard.jsx
+++ src/features/schedule/components/SessionCard.jsx
@@ -1,7 +1,50 @@
-export default function SessionCard() {
+import { LuPlay } from "react-icons/lu";
+import { RiEditLine, RiDeleteBin5Line } from "react-icons/ri";
+
+import Group from "/src/components/Group";
+import Menus from "/src/components/Menus";
+import Paragraph from "/src/ui/Paragraph";
+import Card from "/src/ui/Card";
+import Flex from "/src/ui/Flex";
+
+export default function SessionCard({title, count, estimatedTime, onPlay, onEdit, onDelete}) {
     return (
-        <div>
-            
-        </div>
+        <Group classname={"h-190 space-y-3 overflow-y-scroll"}>
+          <Card>
+            <Flex variant="between">
+              <Flex variant="center">
+                <Group classname={"h-4 w-4 rounded-full bg-gradient-to-r from-slate-200 to-slate-300 dark:from-slate-600 dark:to-slate-700"}>
+                </Group>
+                <Flex classname={"flex-col"}>
+                  <Paragraph classname={"font-medium text-slate-900 dark:text-white"} >{title}</Paragraph>
+                  <Paragraph classname="text-sm text-slate-500 dark:text-slate-400">
+                    {count} cards • {estimatedTime} min
+                  </Paragraph>
+                </Flex>
+              </Flex>
+
+              <Group>
+                <Menus>
+                  <Menus.Toggle />
+
+                  <Menus.Lists>
+                    <Menus.Buttons onClick={onPlay}>
+                      <LuPlay className="w-4 h-4" />
+                      Play
+                    </Menus.Buttons>
+                    <Menus.Buttons onClick={onEdit}>
+                      <RiEditLine className="w-4 h-4" />
+                      Edit
+                    </Menus.Buttons>
+                    <Menus.Buttons onClick={onDelete}>
+                      <RiDeleteBin5Line className="w-4 h-4" />
+                      Delete
+                    </Menus.Buttons>
+                  </Menus.Lists>
+                </Menus>
+              </Group>
+            </Flex>
+          </Card>
+      </Group>
     );
 }
