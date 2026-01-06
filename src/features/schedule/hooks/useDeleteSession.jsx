e85b2df29856cba25eeb02fb41f64c78149f5d8d
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-06 21:44:09 +0000

    Update the session delete hook

--- src/features/schedule/hooks/useDeleteSession.jsx
+++ src/features/schedule/hooks/useDeleteSession.jsx
@@ -1,3 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+import { deleteSession as deleteSessionApi } from "/src/service/apiSchedule";
+
 export default function useDeleteSession() {
+    const queryClient = useQueryClient();
     
+    const { isLoading: isDeleting, mutate: deleteSession } = useMutation({
+        mutationFn: (id) => deleteSessionApi(id),
+        onSuccess: () => {
+          toast.success("Session successfully deleted");
+          queryClient.invalidateQueries({
+            queryKey: ["sessions"],
+          });
+        },
+        onError: (err) => toast.error(err.message),
+    });
+    
+    return { isDeleting, deleteSession };
 }
