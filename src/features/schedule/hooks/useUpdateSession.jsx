389a9a7f40a9ab56c221972bcdf4a8c8ebe022f8
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-08 14:06:05 +0000

    Update in the useUpdateSession hook

--- src/features/schedule/hooks/useUpdateSession.jsx
+++ src/features/schedule/hooks/useUpdateSession.jsx
@@ -1,3 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import { updateSession as updateSessionAPI } from "/src/service/apiSchedule";
+
 export default function useUpdateSession() {
-    
+    const queryClient = useQueryClient();
+
+    const { mutate: updateMutation, isPending: isUpdating } = useMutation({
+        mutationFn: ({ id, data }) => updateSessionAPI(id, data),
+        onSuccess: () => {
+            toast.success("Session successfully updated");
+            queryClient.invalidateQueries({ queryKey: ["sessions"] });
+        },
+        onError: (err) => toast.error(err.message),
+    });
+
+    return { updateMutation, isUpdating };
 }
