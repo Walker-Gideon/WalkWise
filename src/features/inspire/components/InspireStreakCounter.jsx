d6bd8c785353e1056300323d93b97e36778b839d
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-18 21:24:22 +0000

    Update the InspireStreakCount with the user streak

--- src/features/inspire/components/InspireStreakCounter.jsx
+++ src/features/inspire/components/InspireStreakCounter.jsx
@@ -1,10 +1,16 @@
 import { LuFlame } from "react-icons/lu";
 
+import Conditional from "/src/components/Conditional";
 import Paragraph from "/src/ui/Paragraph";
 import SpanText from "/src/ui/SpanText";
 import Card from "/src/components/Card";
 
+import { useUserData } from "/src/user/hook/useUserData";
+
 export default function InspireStreakCounter() {
+  const { userData } = useUserData();
+  const streak = userData ? userData?.streakCount : 0;
+
   return (
     <Card classname={"text-center"}>
       <SpanText>
@@ -13,16 +19,16 @@ export default function InspireStreakCounter() {
       <Paragraph
         classname={"text-4xl font-bold primary-text-color my-2"}
       >
-        {/* {userData.streakCount} */} X
+        {streak}
       </Paragraph>
       <Paragraph classname={"text-lg primary-text-color"}>
         Day Streak!
       </Paragraph>
-      {/* {userData.streakCount !== 0 && ( */}
-      <Paragraph variant="small" classname={"secondary-text-color"}>
-        Don’t break your streak!
-      </Paragraph>
-      {/* )} */}
+      <Conditional condition={streak !== 0}>
+        <Paragraph variant="small" classname={"secondary-text-color"}>
+          Don’t break your streak!
+        </Paragraph>
+      </Conditional>
     </Card>
   );
 }
