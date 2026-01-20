572d1f904de9c03f3a5e8c600ef32ee4b4785eea
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-20 20:19:18 +0000

    Update in the praises and adjustment in the InspirePraises

--- src/features/inspire/components/InspirePraises.jsx
+++ src/features/inspire/components/InspirePraises.jsx
@@ -6,54 +6,6 @@ import SpanText from "/src/ui/SpanText";
 import Card from "/src/components/Card";
 import Group from "/src/ui/Group";
 
-const inspirePraises = [
-  {
-    text: "Your biggest deck shows real dedication to that topic."
-  }, 
-  {
-    text: "You've studied over an hour today — great concentration."
-  },
-  {
-    text: "Studying late into the night — your effort won’t go unnoticed."
-  },
-  {
-    text: "You've mastered {masteredFlashcards} flashcards — your memory is sharp."
-  },
-  {
-    text: "Stay consistent. Your future self will thank you."
-  },
-  {
-    text: "You've achieved 90% mastery in {topSubject} — excellent progress."
-  },
-  {
-    text: "One more day and you'll hit a new streak record! Stay focused."
-  },
-  {
-    text: "The {largestDeck} set is growing fast. Keep going."
-  },
-  {
-    text: "{masteredFlashcards} cards down, so many concepts conquered."
-  },
-  {
-    text: "You've reached {totalFlashcards} flashcards — you're making progress."
-  },
-  {
-    text: "You've achieved {totalSessions} study sessions — keep it up!"
-  },
-  {
-    text: "You've reached {totalStreak} study streaks — you're on fire!"
-  },
-  {
-    text: "Your study time today shows real dedication."
-  },
-  {
-    text: "Burning the midnight oil? Night owls like you thrive in silence."
-  },
-  {
-    text: "You've reached {totalFlashcards} flashcards — you're making progress."
-  },
-]
-
 export default function InspirePraises() {
   return (
     <Card>
