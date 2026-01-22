4d95bbfe962d2aab5577f2e5f365c6ee06ad87b3
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-22 21:58:36 +0000

    Update the quotes with quote and author

---
+++ src/data/quote.js
@@ -0,0 +1,84 @@
+const quotes = [
+  {
+    quote: "Education is the most powerful weapon which you can use to change the world.",
+    author: "Nelson Mandela"
+  },
+  {
+    quote: "The beautiful thing about learning is that no one can take it away from you.",
+    author: "B.B. King"
+  },
+  {
+    quote: "Education is not the filling of a pail, but the lighting of a fire.",
+    author: "William Butler Yeats"
+  },
+  {
+    quote: "An investment in knowledge pays the best interest.",
+    author: "Benjamin Franklin"
+  },
+  {
+    quote: "The roots of education are bitter, but the fruit is sweet.",
+    author: "Aristotle"
+  },
+  {
+    quote: "Change is the end result of all true learning.",
+    author: "Leo Buscaglia"
+  },
+  {
+    quote: "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
+    author: "Malcolm X"
+  },
+  {
+    quote: "Develop a passion for learning. If you do, you will never cease to grow.",
+    author: "Anthony J. D'Angelo"
+  },
+  {
+    quote: "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+    author: "Dr. Seuss"
+  },
+  {
+    quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    quote: "Learning never exhausts the mind.",
+    author: "Leonardo da Vinci"
+  },
+  {
+    quote: "I am always doing that which I cannot do, in order that I may learn how to do it.",
+    author: "Pablo Picasso"
+  },
+  {
+    quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    author: "Benjamin Franklin"
+  },
+  {
+    quote: "The capacity to learn is a gift; the ability to learn is a skill; the willingness to learn is a choice.",
+    author: "Brian Herbert"
+  },
+  {
+    quote: "Whatever you think that you will be. If you think yourself weak, weak you will be; if you think yourself strong, strong you will be.",
+    author: "Swami Vivekananda"
+  },
+  {
+    quote: "He who opens a school door, closes a prison.",
+    author: "Victor Hugo"
+  },
+  {
+    quote: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe"
+  },
+  {
+    quote: "Education is what remains after one has forgotten what one has learned in school.",
+    author: "Albert Einstein"
+  },
+  {
+    quote: "The mind is not a vessel to be filled, but a fire to be kindled.",
+    author: "Plutarch"
+  },
+  {
+    quote: "A person who never made a mistake never tried anything new.",
+    author: "Albert Einstein"
+  }
+];
+
+export default quotes;
