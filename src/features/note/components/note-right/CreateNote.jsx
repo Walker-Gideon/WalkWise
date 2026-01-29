d6aa73f1703c3d5b4faec5dd8c81b37545434607
Walker-Gideon <gideonawortwe3@gmail.com>
2026-01-29 20:11:48 +0000

    Update in the CreateNote

--- src/features/note/components/note-right/CreateNote.jsx
+++ src/features/note/components/note-right/CreateNote.jsx
@@ -1,4 +1,9 @@
-// import { EditorContent } from "@tiptap/react"; /*Install this dependence */
+import TextAlign from "@tiptap/extension-text-align";
+import Highlight from "@tiptap/extension-highlight";
+import Underline from "@tiptap/extension-underline";
+import { EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { useEditor } from "@tiptap/react";
 
 import CreateNoteEditor from "./CreateNoteEditor";
 import Container from "/src/ui/Container";
@@ -7,6 +12,29 @@ import Input from "/src/ui/Input";
 import Box from "/src/ui/Box";
 
 export default function CreateNote() {
+
+  // Editing note
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Highlight,
+      Underline,
+    ],
+    content,
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML();
+      if (readAlredyNote) {
+        setCurrentNote((prev) => ({ ...prev, content: html }));
+      } else {
+        setContent(html);
+      }
+    },
+  });
+
+
   return (
     <Container adjust={true} classname={""}>
       <CreateNoteEditor />
@@ -35,7 +63,7 @@ export default function CreateNote() {
             "mt-4 h-full flex-grow text-gray-900 placeholder:text-gray-500 dark:text-white dark:placeholder:text-gray-400"
           }
         >
-          {/* <EditorContent editor={editor} /> */}
+          <EditorContent editor={editor} />
         </Box>
       </Group>
     </Container>
