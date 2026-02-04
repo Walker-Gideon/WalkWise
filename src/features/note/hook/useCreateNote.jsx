c223215d30e091e76223a2093ee70df8df21cd76
Walker-Gideon <gideonawortwe3@gmail.com>
2026-02-04 16:27:02 +0000

    Update the create note hook

--- src/features/note/hook/useCreateNote.jsx
+++ src/features/note/hook/useCreateNote.jsx
@@ -1,6 +1,29 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
+import { createNote as createNoteAPI } from "/src/service/apiNote";
+import { useUserData } from "/src/user/hook/useUserData";
+
 export default function useCreateNote() {
-    return {}
+    const queryClient = useQueryClient();
+
+    const { firebaseUser } = useUserData();
+    const userId = firebaseUser?.uid;
+
+    const { mutate: createNote, isPending: isCreating } = useMutation({
+        mutationFn: (data) => {
+            if (!userId) {
+                toast.error("You must be logged in to create a note.");
+            }
+
+            return createNoteAPI({ ...data, userId });
+        },
+        onSuccess: () => {
+            toast.success("Note created successfully");
+            queryClient.invalidateQueries({ queryKey: ["notes", userId] });
+        },
+        onError: (err) => toast.error(err.message),
+    });
+
+    return { createNote, isCreating };
 }
