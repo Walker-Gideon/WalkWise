135ac7b1bdf94ae02048743749c804c433b67b35
Walker-Gideon <gideonawortwe3@gmail.com>
2026-02-10 16:11:32 +0000

    Implemented the delete note logic

--- src/features/note/components/note-left/NoteDisplay.jsx
+++ src/features/note/components/note-left/NoteDisplay.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { RiDeleteBin5Line } from "react-icons/ri";
 
+import ConfirmDelete from "/src/components/ConfirmDelete";
+import Conditional from "/src/components/Conditional";
 import HeaderText from "/src/ui/HeaderText";
 import Container from "/src/ui/Container";
 import Paragraph from "/src/ui/Paragraph";
@@ -10,6 +12,7 @@ import Button from "/src/ui/Button";
 import Flex from "/src/ui/Flex";
 
 import useFormattedDate from "/src/hook/useFormattedDate";
+import useDeleteNote from "../../hook/useDeleteNote";
 import { useNote } from "../../context/NoteContext";
 
 function NoteDate({ createdAt }) {
@@ -19,8 +22,12 @@ function NoteDate({ createdAt }) {
 
 export default function NoteDisplay({ notes }) {
   const { query } = useNote();
+  const { isDeleting, deleteNote } = useDeleteNote();
 
+  const [selectedId, setSelectedId] = useState(null);
   const [isSearching, setIsSearching] = useState(false);
+  const [isDeleteModal, setIsDeleteModal] = useState(false);
+  const [selectedNoteTitle, setSelectedNoteTitle] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
@@ -42,6 +49,22 @@ export default function NoteDisplay({ notes }) {
     setSearchParams({ noteId: id });
   }
 
+  function handleCloseModel() {
+    setIsDeleteModal(false);
+  }
+
+  function handleDeleteClick(id, title) {
+    setSelectedId(id);
+    setSelectedNoteTitle(title);
+  }
+
+  function handleConfirmDelete() {
+    if (selectedId) {
+      deleteNote(selectedId);
+      handleCloseModel();
+    }
+  }
+
   if(isSearching) {
     return (
       <Flex variant="center" classname={"h-full"}>
@@ -51,39 +74,52 @@ export default function NoteDisplay({ notes }) {
   }
 
   return (
-    <Container adjust={true} classname={"h-full overflow-y-auto"}>
-      {!isSearching && filtereNote.map((note) => (
-        <div
-          key={note.id}
-          className={`my-1 flex w-full cursor-pointer items-center justify-between gap-2 border-b border-stone-300 ${noteId === note.id ? "bg-slate-300 dark:bg-slate-700" : ""}`}
-        >
-          <div 
-            role="button" 
-            className="w-full py-2 pl-4"
-            onClick={() => handleDisplayNote(note.id)} 
+    <>
+      <Container adjust={true} classname={"h-full overflow-y-auto"}>
+        {!isSearching && filtereNote.map((note) => (
+          <div
+            key={note.id}
+            className={`my-1 flex w-full cursor-pointer items-center justify-between gap-2 border-b border-stone-300 ${noteId === note.id ? "bg-slate-300 dark:bg-slate-700" : ""}`}
           >
-            <HeaderText
+            <div 
+              role="button" 
+              className="w-full py-2 pl-4"
+              onClick={() => handleDisplayNote(note.id)} 
+            >
+              <HeaderText
+                variant="secondary"
+                classname={"w-40 truncate whitespace-nowrap primary-text-color"}
+              >
+                {note.title}
+              </HeaderText>
+              <Paragraph
+                variant="small"
+                classname={"secondary-text-color text-xs"}
+              >
+                <NoteDate createdAt={note.createdAt} />
+              </Paragraph>
+            </div>
+            <Button
               variant="secondary"
-              classname={"w-40 truncate whitespace-nowrap primary-text-color"}
+              onclick={() => {
+                handleDeleteClick(note.id, note.title);
+                setIsDeleteModal(true);
+              }}
+              classname={"text-slate-700 dark:text-slate-100 pr-4"}
             >
-              {note.title}
-            </HeaderText>
-            <Paragraph
-              variant="small"
-              classname={"secondary-text-color text-xs"}
-            >
-              <NoteDate createdAt={note.createdAt} />
-            </Paragraph>
+              <RiDeleteBin5Line className="h-5 w-5" />
+            </Button>
           </div>
-          <Button
-            variant="secondary"
-            onclick={() => {}}
-            classname={"text-slate-700 dark:text-slate-100 pr-4"}
-          >
-            <RiDeleteBin5Line className="h-5 w-5" />
-          </Button>
-        </div>
-      ))}
-    </Container>
+        ))}
+      </Container>
+      <Conditional condition={isDeleteModal}>
+        <ConfirmDelete
+          disabled={isDeleting}
+          onCloseModal={handleCloseModel}
+          onConfirm={handleConfirmDelete}
+          resourceName={selectedNoteTitle}
+        />
+      </Conditional>
+    </>
   );
 }
