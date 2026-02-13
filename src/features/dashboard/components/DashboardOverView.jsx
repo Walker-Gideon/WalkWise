0fa488c3e1ade86bd5052e2e94e58d15e059d5b1
Walker-Gideon <gideonawortwe3@gmail.com>
2026-02-13 23:19:44 +0000

    Fix DashboardOverView bug

--- src/features/dashboard/components/DashboardOverView.jsx
+++ src/features/dashboard/components/DashboardOverView.jsx
@@ -52,7 +52,7 @@ function FlashcardItem({ flashcard }) {
         <Group classname={"flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-slate-200 to-slate-300 dark:from-slate-600 dark:to-slate-700"}></Group>
         <Group>
           <Paragraph classname={"font-medium primary-text-color"}>
-            {flashcard.tags || "Untitled Flashcard"}
+            {flashcard.title || "Untitled Flashcard"}
           </Paragraph>
           <Paragraph variant="small" classname={"secondary-text-color"}>
             {cardCount} card{cardCount !== 1 ? "s" : ""}
