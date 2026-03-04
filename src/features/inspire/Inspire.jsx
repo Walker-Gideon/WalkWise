4e5f420296ed49c2fa0e362ff127e27b5df2b32a
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-04 16:21:58 +0000

    Fixes on hight and scroll responsiveness in the Inspire section

--- src/features/inspire/Inspire.jsx
+++ src/features/inspire/Inspire.jsx
@@ -1,14 +1,14 @@
 import { LuAward, LuLightbulb } from "react-icons/lu";
 
-import InspireStreakCounter from "./components/InspireStreakCounter";
-import InspireAchievement from "./components/InspireAchievement";
-import InspireProgress from "./components/InspireProgress";
-import InspireActions from "./components/InspireActions";
-import InspireHeader from "./components/InspireHeader";
-import Motivation from "/src/components/Motivation";
-import Container from "/src/ui/Container";
-import Group from "/src/ui/Group";
 import Main from "/src/ui/Main";
+import Group from "/src/ui/Group";
+import Container from "/src/ui/Container";
+import Motivation from "/src/components/Motivation";
+import InspireHeader from "./components/InspireHeader";
+import InspireActions from "./components/InspireActions";
+import InspireProgress from "./components/InspireProgress";
+import InspireAchievement from "./components/InspireAchievement";
+import InspireStreakCounter from "./components/InspireStreakCounter";
 
 import { useQuotes } from "/src/hook/useQuotes";
 
@@ -16,11 +16,11 @@ export default function Inspire() {
   const {quote, author} = useQuotes();
 
   return (
-    <Container classname={""}>
+    <Container classname={"flex h-full flex-col"}>
       <InspireHeader />
       <Main
         classname={
-          "grid grid-cols-1 gap-x-6 gap-y-3 lg:grid-cols-3 medium:h-[510px] h-screen overflow-y-scroll p-6"
+          "grid min-h-0 flex-1 grid-cols-1 gap-x-6 gap-y-3 overflow-y-auto p-6 lg:grid-cols-3"
         }
       >
         <Group classname={"space-y-6 lg:col-span-2"}>
