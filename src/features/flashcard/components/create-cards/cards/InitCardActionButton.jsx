5e10a2ff4916323e7a8af872b9bffc0d6cd97193
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-10 14:04:32 +0000

    Update the InitCardActionButton component

--- src/features/flashcard/components/create-cards/cards/InitCardActionButton.jsx
+++ src/features/flashcard/components/create-cards/cards/InitCardActionButton.jsx
@@ -1,3 +1,19 @@
+import Group from "/src/ui/Group";
+import Button from "/src/ui/Button";
+
+import useToggleDisplay from "/src/hook/useToggleDisplay";
+import { useFlashcard } from "../../../context/FlashcardContext";
+
 export default function InitCardActionButton() {
+    const { setIsDisplay } = useFlashcard();
     
+    const handleToggleDisplay = useToggleDisplay(setIsDisplay);
+
+    return (
+        <Group classname={"space-x-2 hidden md:block"}>
+            <Button type="colors" onclick={handleToggleDisplay}>
+                Create Flashcard
+            </Button>
+        </Group>
+    );
 }
