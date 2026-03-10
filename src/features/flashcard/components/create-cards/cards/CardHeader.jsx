5df16427b56f089f388ae7fd052c3e1f08fa5fab
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-10 14:12:56 +0000

    Update in both the CardHeader and InitCardActionButton components

--- src/features/flashcard/components/create-cards/cards/CardHeader.jsx
+++ src/features/flashcard/components/create-cards/cards/CardHeader.jsx
@@ -1,26 +1,14 @@
-import Group from "/src/ui/Group";
-import Button from "/src/ui/Button";
+import InitCardActionButton from "./InitCardActionButton";
 import Heading from "/src/components/Heading";
 
-import useToggleDisplay from "/src/hook/useToggleDisplay";
-import { useFlashcard } from "../../../context/FlashcardContext";
-
 export default function CardHeader() {
-  const { setIsDisplay } = useFlashcard();
-
-  const handleToggleDisplay = useToggleDisplay(setIsDisplay);
-
   return (
     <Heading
       headerText="My Flashcards"
       paragraphText="Reinforce your memory, one card at a time."
       theme={true}
     >
-      <Group classname={"space-x-2 hidden md:block"}>
-        <Button type="colors" onclick={handleToggleDisplay}>
-          Create Flashcard
-        </Button>
-      </Group>
+      <InitCardActionButton styling={false} />
     </Heading>
   );
 }
--- src/features/flashcard/components/create-cards/cards/InitCardActionButton.jsx
+++ src/features/flashcard/components/create-cards/cards/InitCardActionButton.jsx
@@ -4,13 +4,13 @@ import Button from "/src/ui/Button";
 import useToggleDisplay from "/src/hook/useToggleDisplay";
 import { useFlashcard } from "../../../context/FlashcardContext";
 
-export default function InitCardActionButton() {
+export default function InitCardActionButton({ styling }) {
     const { setIsDisplay } = useFlashcard();
     
     const handleToggleDisplay = useToggleDisplay(setIsDisplay);
 
     return (
-        <Group classname={"space-x-2 hidden md:block"}>
+        <Group classname={`space-x-2 ${styling ? "" : "hidden md:block"}`}>
             <Button type="colors" onclick={handleToggleDisplay}>
                 Create Flashcard
             </Button>
