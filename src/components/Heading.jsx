600bcc613e43e3a6ca9bc6986b477bfac66c851b
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-18 22:42:48 +0000

    Update the Heading component with the user profile image

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,3 +1,6 @@
+import { LuUser } from "react-icons/lu";
+
+import Box from "/src/ui/Box";
 import Flex from "/src/ui/Flex";
 import Group from "/src/ui/Group";
 import Header from "/src/ui/Header";
@@ -5,6 +8,8 @@ import Conditional from "./Conditional"
 import Paragraph from "/src/ui/Paragraph";
 import HeaderText from "/src/ui/HeaderText";
 import MenuButton from "/src/navigation/components/MenuButton";
+
+import { useUserData } from "/src/user/hook/useUserData";
 
 export default function Heading({
   theme,
@@ -17,15 +22,17 @@ export default function Heading({
   paragraphStyling,
   menu = true,
 }) {
+  const { userData } = useUserData();
+
   return (
     <Header
       classname={`sticky top-0 z-30 border-b border-stone-300 shadow-sm backdrop-blur-sm  ${classname} ${theme ? "defaultColor dark:border-slate-700" : "bg-white/30"}`}
     >
-      <Flex classname={`w-full py-3 medium:py-4 ${menu ? "pl-4" : "px-6"}`}>
+      <Flex classname={`w-full py-3 medium:py-4 ${menu ? "pl-4 pr-6 items-center" : "px-6"}`}>
         <Conditional condition={menu}>
           <MenuButton />
         </Conditional>
-        <Flex variant="between" classname={`w-full ${menu ? "pl-4 pr-6" : ""}`}>
+        <Flex variant="between" classname={`w-full ${menu ? "pl-2" : ""}`}>
           <Group>
             <div className="block md:hidden">
               <Small 
@@ -44,6 +51,32 @@ export default function Heading({
           </Group>
           <Group classname={groupStyling}>{children}</Group>
         </Flex>
+        {/* Here we need to add the user profile */}
+        <Conditional condition={menu}>
+          <div>
+            <Box
+              adjustWidth={true}
+              classname={
+                "rounded-full flex items-center justify-center h-12 w-12 border-2 borderStyling md:hidden"
+              }
+              >
+                <Conditional condition={!userData?.photoURL}>
+                  <LuUser
+                    className={`h-5 w-5 text-slate-500 dark:text-white`}
+                  />
+                </Conditional>
+                <Conditional condition={userData?.photoURL}>
+                  <div>
+                  <img
+                    src={userData?.photoURL}
+                    alt="User profile"
+                    className="h-15 w-15 rounded-full object-cover"
+                  /> 
+                  </div>
+                </Conditional>
+            </Box>
+          </div>
+        </Conditional>
       </Flex>
     </Header>
   );
--- src/features/dashboard/components/DashboardHeader.jsx
+++ src/features/dashboard/components/DashboardHeader.jsx
@@ -1,14 +1,15 @@
 import { LuFlame } from "react-icons/lu";
 
-import Heading from "/src/components/Heading";
-import SpanText from "/src/ui/SpanText";
-import Group from "/src/ui/Group";
 import Flex from "/src/ui/Flex";
+import Group from "/src/ui/Group";
+import SpanText from "/src/ui/SpanText";
+import Heading from "/src/components/Heading";
 
 import { useUserData } from "/src/user/hook/useUserData";
 
 export default function DashboardHeader() {
   const { userData } = useUserData();
+
   const username = userData ? `${userData?.username}` : "username";
   const streak = userData ? userData?.streakCount : 0;
 
@@ -21,7 +22,7 @@ export default function DashboardHeader() {
       <Flex classname={"items-center space-x-3"}>
         <Group
           classname={
-            "medium:flex hidden items-center justify-center gap-2 rounded-full px-3 py-2 whitespace-nowrap transition bg-gradient-to-r from-slate-200 to-slate-300 dark:from-slate-600 dark:to-slate-700"
+            "md:flex hidden items-center justify-center gap-2 rounded-full px-3 py-2 whitespace-nowrap transition bg-gradient-to-r from-slate-200 to-slate-300 dark:from-slate-600 dark:to-slate-700"
           }
         >
           <LuFlame className="mb-1 h-4 w-4 text-slate-600 dark:text-slate-300" />
