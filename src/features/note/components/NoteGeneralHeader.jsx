5d73d228d1300b62570b74d80769816306e72239
Walker-Gideon <gideonawortwe3@gmail.com>
2026-03-28 17:22:28 +0000

    Created the NoteGeneralHeader component

---
+++ src/features/note/components/NoteGeneralHeader.jsx
@@ -0,0 +1,11 @@
+import Heading from "/src/components/Heading";
+
+export default function NoteGeneralHeader() {
+    return (
+        <Heading
+            headerText="My Notes"
+            paragraphText="Organize your thoughts and ideas."
+            theme={true}
+        />
+    )
+}
