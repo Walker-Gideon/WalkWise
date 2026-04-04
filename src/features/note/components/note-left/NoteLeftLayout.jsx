97026f1e9fad49a95c08772c21eb20d44af11bbc
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-04 16:25:57 +0000

    Implement component switching in the note section for mobile view

--- src/features/note/components/note-left/NoteLeftLayout.jsx
+++ src/features/note/components/note-left/NoteLeftLayout.jsx
@@ -1,17 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
 import Container from "/src/ui/Container";
 import LeftNoteHeader from "./LeftNoteHeader";
 import LeftNoteContent from "./LeftNoteContent";
 
-import useFetchNotes from "../../hook/useFetchNotes";
+import { useNote } from "../../context/NoteContext";
 
 export default function NoteLeftLayout() {
-  const { notes } = useFetchNotes();
+  const { isDisplayNote } = useNote();
+  const [searchParams] = useSearchParams();
 
-  const hasNotes = notes?.length > 0;
+  const noteId = searchParams.get("noteId");
+  const isEditorOpen = isDisplayNote || noteId;
+  const hiddenOnMobile = isEditorOpen ? "hidden maxmid:flex" : "flex maxmid:flex";
 
   return (
     <Container
-      classname={`maxmid:flex h-screen flex-col border-r-0 borderStyling medium:border-r ${!hasNotes ? "hidden" : ""}`}
+      classname={`${hiddenOnMobile} h-screen flex-col border-r-0 borderStyling medium:border-r`}
     >
       <LeftNoteHeader />
       <LeftNoteContent />
