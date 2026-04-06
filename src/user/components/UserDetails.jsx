c4134fa4763abd5bd1b95995544ddf5edd7d0ece
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-06 19:16:25 +0000

    Update UserDetails component

--- src/user/components/UserDetails.jsx
+++ src/user/components/UserDetails.jsx
@@ -1,3 +1,23 @@
-export default function UserDetails() {
-    return <div></div>;
+import Group from "/src/ui/Group";
+import Paragraph from "/src/ui/Paragraph";
+
+import { useUserData } from "../hook/useUserData";
+
+export default function UserDetails({ classname1, classname2 }) {
+    const { userData } = useUserData();
+
+    return (
+        <Group>
+            <Paragraph
+                classname={`font-bold whitespace-nowrap truncate w-30 ${classname1}`}
+            >
+                {!userData ? "username" : userData?.username}
+            </Paragraph>
+            <Paragraph
+                classname={`text-xs font-medium truncate w-30 ${classname2}`}
+            >
+                {!userData ? "example123@gmail.com" : userData?.email}
+            </Paragraph>
+        </Group>
+    );
 }
