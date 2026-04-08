ce7cc925815cd0597909ffc2492a1a74c9a3e0b9
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-08 23:45:18 +0000

    Update in the Profile and MainNav component

--- src/user/components/Profile.jsx
+++ src/user/components/Profile.jsx
@@ -1,32 +1,27 @@
-import { LuUser } from "react-icons/lu";
+import { NavLink } from "react-router-dom";
 
-import Box from "/src/ui/Box";
 import Group from "/src/ui/Group";
-import UserDetails from "./UserDetails";
+import UserImage from "./UserImage";
+import SpanText from "/src/ui/SpanText";
 
 import { useNav } from "/src/contexts/NavigationContext";
 
 export default function Profile() {
-    const { isExpanded } = useNav();
+    const { isExpanded, handleToggle } = useNav();
     
     return (
-        <Group classname={`group relative inline-flex`}>
-            <Box
-                adjustWidth={true}
-                classname={
-                    "rounded-full flex items-center justify-center w-9 h-9 border-2 borderStyling"
-                }
-            >
-                <LuUser className={`h-5 w-5 text-slate-500 dark:text-white`} />
-            </Box>
+        <NavLink
+            to="/settings"
+            end
+            onClick={handleToggle}
+            className={"group relative inline-flex"}
+        >
+            <UserImage showOnMobile={true} hideOnMobile={true} />
             <Group
-                classname={`pointer-events-none absolute top-0.5 left-12 z-50 -translate-y-1/2 transform rounded-sm bg-slate-500 px-2 py-1 text-sm font-semibold whitespace-nowrap text-white opacity-0 transition-opacity group-hover:opacity-100 z-50 ${isExpanded ? "medium:block" : "hidden"}`}
+                classname={`pointer-events-none absolute top-0.5 left-12 z-40 -translate-y-1/2 transform rounded-sm bg-slate-500 px-2 py-1 text-xs font-semibold whitespace-nowrap text-white opacity-0 transition-opacity group-hover:opacity-100 ${isExpanded ? "medium:block" : "hidden"}`}
             >
-                <UserDetails
-                    classname1={"text-slate-50"}
-                    classname2={"text-slate-50"}
-                />
+                <SpanText>Profile</SpanText>
             </Group>
-        </Group>
+        </NavLink>
     );
 }
