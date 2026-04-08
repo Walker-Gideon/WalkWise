e4bfe700b3b705d2f71dda713f3df3b0c1d2bff8
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-08 23:57:38 +0000

    Refactoring in the Profile component

--- src/user/components/Profile.jsx
+++ src/user/components/Profile.jsx
@@ -1,5 +1,3 @@
-import { NavLink } from "react-router-dom";
-
 import Group from "/src/ui/Group";
 import UserImage from "./UserImage";
 import SpanText from "/src/ui/SpanText";
@@ -7,21 +5,16 @@ import SpanText from "/src/ui/SpanText";
 import { useNav } from "/src/contexts/NavigationContext";
 
 export default function Profile() {
-    const { isExpanded, handleToggle } = useNav();
+    const { isExpanded } = useNav();
     
     return (
-        <NavLink
-            to="/settings"
-            end
-            onClick={handleToggle}
-            className={"group relative inline-flex"}
-        >
+        <Group classname={`group relative inline-flex`}>
             <UserImage showOnMobile={true} hideOnMobile={true} />
             <Group
                 classname={`pointer-events-none absolute top-0.5 left-12 z-40 -translate-y-1/2 transform rounded-sm bg-slate-500 px-2 py-1 text-xs font-semibold whitespace-nowrap text-white opacity-0 transition-opacity group-hover:opacity-100 ${isExpanded ? "medium:block" : "hidden"}`}
             >
                 <SpanText>Profile</SpanText>
             </Group>
-        </NavLink>
+        </Group>
     );
 }
