d7ea53b0c2b4aeb8b614a78b232e58adf52fbd1f
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-20 19:00:01 +0000

    Adjustment in the AdditionalInformation section

--- src/landingPage/additionalInfo/AdditionalInformation.jsx
+++ src/landingPage/additionalInfo/AdditionalInformation.jsx
@@ -1,19 +1,28 @@
 import Container from "/src/ui/Container";
-import additionaInformationData from "/src/data/additionalInformationData.js";
 import InformationContent from "./components/InformationContent";
+
+import additionaInformationData from "/src/data/additionalInformationData.js";
 
 export default function AdditionalInformation() {
   return (
-    <Container adjust={true} id="features" classname={"md:px-2 lg:px-16 px-4"}>
+    <Container 
+      adjust={true} 
+      id="features" 
+      classname={"md:px-8 lg:px-16 px-6"}
+    >
       {additionaInformationData.map((data, index) => (
-        <Container key={index} adjust={true} classname={`my-25 medium:my-30 ${index === 0 || index === 2 ? `w-full rounded-4xl bg-slate-50` : ``}`}>
+        <Container 
+          key={index} 
+          adjust={true} 
+          classname={`my-25 medium:my-30 ${index === 0 || index === 2 ? `w-full rounded-4xl bg-slate-50` : ``}`}
+        >
           <InformationContent
-            index={index}
             icon={<data.icon className={`h-5 w-5 text-slate-600`} />}
-            title={data.title}
-            subtitle={data.subtitle}
-            points={data.points}
+            index={index}
             url={data.url}
+            title={data.title}
+            points={data.points}
+            subtitle={data.subtitle}
           />
         </Container>
       ))}
--- src/landingPage/additionalInfo/components/InformationContent.jsx
+++ src/landingPage/additionalInfo/components/InformationContent.jsx
@@ -1,27 +1,42 @@
-import InformationTextContent from "./InformationTextContent";
-import Container from "/src/ui/Container";
-import Image from "/src/ui/Image";
 import Box from "/src/ui/Box";
+import Image from "/src/ui/Image";
+import Container from "/src/ui/Container";
+import InformationTextContent from "./InformationTextContent";
 
 export default function InformationContent({
-  index,
+  url,
   icon,
   title,
-  subtitle,
+  index,
   points,
-  url,
+  subtitle,
 }) {
   return (
     <Container
       adjust={true}
-      classname={`px-2 py-4 md:px-6 md:py-0 lg:px-15`}
+      classname={"px-2 py-4 md:px-6 md:py-0 lg:px-15"}
     >
-      <Container adjust={true} classname="flex flex-col-reverse gap-8 md:grid md:h-100 md:grid-cols-2 md:items-center lg:gap-20">
-        <InformationTextContent index={index} icon={icon} title={title} subtitle={subtitle} points={points} />
+      <Container 
+        adjust={true} 
+        classname={"flex flex-col-reverse gap-8 md:grid md:h-100 md:grid-cols-2 md:items-center lg:gap-20"}
+      >
+        <InformationTextContent 
+          icon={icon} 
+          index={index} 
+          title={title} 
+          points={points} 
+          subtitle={subtitle} 
+        />
 
-        <Box adjustWidth={true} classname={`object-cover ${index === 1 ? `md:order-1` : ``} ${index === 3 ? `md:order-1` : ``}`}>
-          <Image src={url} alt="image"
-          classname="w-full rounded-2xl object-scale-down shadow-md shadow-slate-600" />
+        <Box 
+          adjustWidth={true} 
+          classname={`object-cover ${index === 1 ? `md:order-1` : ``} ${index === 3 ? `md:order-1` : ``}`}
+        >
+          <Image 
+            src={url} 
+            alt="image"
+            classname={"w-full rounded-2xl object-scale-down shadow-lg shadow-slate-600"} 
+          />
         </Box>
       </Container>
     </Container>
