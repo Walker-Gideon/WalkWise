7708111d69c89592709ef89a31b6e2fc9a5d65b2
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-22 18:38:30 +0000

    Adjustment in the SignIn Form component

--- src/authentication/sign-in/components/SignInForm.jsx
+++ src/authentication/sign-in/components/SignInForm.jsx
@@ -1,26 +1,25 @@
-import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
-import { FiEyeOff, FiEye } from "react-icons/fi";
+import toast from "react-hot-toast";
 import { LuLoader } from "react-icons/lu";
+import { FiEyeOff, FiEye } from "react-icons/fi";
 
-import Conditional from "/src/components/Conditional";
-import Container from "/src/ui/Container";
-import SpanText from "/src/ui/SpanText";
-import Button from "/src/ui/Button";
 import Form from "/src/ui/Form";
-import Box from "/src/ui/Box";
+import Button from "/src/ui/Button";
+import SpanText from "/src/ui/SpanText";
+import Conditional from "/src/components/Conditional";
+import FormRow from "/src/authentication/components/FormRow";
 
 import { loginUser } from "/src/service/apiAuth";
 
 export default function SignInForm() {
-  const [hidePassword, setHidePassword] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
-
   const navigate = useNavigate();
-  const { handleSubmit, register, reset } = useForm();
+  const { handleSubmit, register, reset, formState: { errors } } = useForm();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [hidePassword, setHidePassword] = useState(true);
 
   async function onSubmit(data) {
     const { email, password } = data;
@@ -47,29 +46,36 @@ export default function SignInForm() {
   const styling = `w-full rounded-sm border border-stone-300 px-1.5 py-1.5 text-sm text-black transition-all duration-300 placeholder:text-xs hover:border-slate-400 focus:ring-2 focus:ring-slate-400 focus:outline-hidden`;
 
   return (
-    <Container adjust={true} classname={"medium:w-80 mt-4 w-70"}>
-      <Form onsubmit={handleSubmit(onSubmit)}>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          placeholder="Email"
-          disabled={isLoading}
-          className={styling}
-          {...register("email", { required: true })}
-        />
-        <Box adjustWidth={true} classname={"relative my-2"}>
+      <Form onsubmit={handleSubmit(onSubmit)} classname={"w-full"}>
+        <FormRow errorsField={errors.email} errorMessage={errors.email?.message}>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            className={styling}
+            {...register("email", { 
+              required: "Email is required", 
+              disabled: isLoading 
+            })}
+          />
+        </FormRow>
+
+        <FormRow errorsField={errors.password} errorMessage={errors.password?.message} classname={"relative mt-2 mb-6"}>
           <input
             id="password"
             type={!hidePassword ? "text" : "password"}
             name="password"
             placeholder="Password"
-            disabled={isLoading}
             className={styling}
-            {...register("password", { required: true })}
+            {...register("password", { 
+              required: "Password is required", 
+              disabled: isLoading 
+            })}
           />
           <Button 
             variant="secondary"
+            ariaLabel="Toggle Password Visibility"
             classname={"absolute top-2.5 right-2"} 
             disabled={isLoading}
             onclick={(e) => {
@@ -78,16 +84,18 @@ export default function SignInForm() {
             }}
           >
             <Conditional condition={hidePassword}>
-              <FiEye className="text-sm" />
+              <FiEye className={"text-sm"} />
             </Conditional>
             <Conditional condition={!hidePassword}>
-              <FiEyeOff className="text-sm" />
+              <FiEyeOff className={"text-sm"} />
             </Conditional>
           </Button>
-        </Box>
+        </FormRow>
+
         <Button
-          type="colors"
           submit={true}
+          type="colors"
+          ariaLabel="Log in"
           disabled={isLoading}
           classname={"w-full flex items-center justify-center"}
         > 
@@ -95,10 +103,9 @@ export default function SignInForm() {
             <SpanText>Log in</SpanText>
           </Conditional>
           <Conditional condition={isLoading}>
-            <LuLoader className="for spinning h-5 w-5 animate-spin" />
+            <LuLoader className={"for spinning h-5 w-5 animate-spin"} />
           </Conditional> 
         </Button>
       </Form>
-    </Container>
   );
 }
