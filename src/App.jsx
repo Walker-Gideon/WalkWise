871e1002599e377a810122eee4caa545ae601ca1
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-22 16:36:45 +0000

    Refactoring of code in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
+import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import ForgetAccount from "./authentication/forget/ForgetAccount";
-import Dashboard from "./features/dashboard/Dashboard";
-import Flashcard from "./features/flashcard/Flashcard";
-import Schedules from "./features/schedule/Schedules";
-import SignIn from "./authentication/sign-in/SignIn";
-import SignUp from "./authentication/sign-up/SignUp";
+import AppLayout from "./ui/AppLayout";
+import Note from "./features/note/Note";
+import PageNotFound from "./ui/PageNotFound";
+import ProtectedRoute from "./ui/ProtectedRoute";
+import Inspire from "./features/inspire/Inspire";
+import Accounts from "./authentication/Accounts";
 import Settings from "./features/settings/Settings";
 import LandingPage from "./landingPage/LandingPage";
 import Verify from "./authentication/verify/Verify";
-import Accounts from "./authentication/Accounts";
-import Inspire from "./features/inspire/Inspire";
-import ProtectedRoute from "./ui/ProtectedRoute";
-import PageNotFound from "./ui/PageNotFound";
-import Note from "./features/note/Note";
-import AppLayout from "./ui/AppLayout";
+import SignUp from "./authentication/sign-up/SignUp";
+import SignIn from "./authentication/sign-in/SignIn";
+import Schedules from "./features/schedule/Schedules";
+import Dashboard from "./features/dashboard/Dashboard";
+import Flashcard from "./features/flashcard/Flashcard";
+import ForgetAccount from "./authentication/forget/ForgetAccount";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +31,7 @@ const queryClient = new QueryClient({
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
 
       <BrowserRouter>
         <Routes>
@@ -80,7 +80,7 @@ export default function App() {
           style: {
             fontSize: "14px",
             maxWidth: "500px",
-            padding: "14px 24px",
+            padding: "8px 20px",
             background: "#fff",
             color: "#374151",
           },
