763385c847e2911e00aca5b029f0bbc4015cc358
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-22 18:39:08 +0000

    Adjustment in the Form close button component

--- src/authentication/components/AuthCloseButton.jsx
+++ src/authentication/components/AuthCloseButton.jsx
@@ -1,12 +1,17 @@
 import { LuX } from "react-icons/lu";
+
 import Flex from "/src/ui/Flex";
 import Button from "/src/ui/Button";
 
 export default function AuthCloseButton() {
   return (
-    <Flex classname="w-full items-end justify-end">
-      <Button to="/">
-        <LuX className="text-2xl font-bold" />
+    <Flex classname={"w-full items-end justify-end px-4"}>
+      <Button 
+        to="/"
+        type="buttonText"
+        aria-label="Close"
+      >
+        <LuX size={30} className={"font-bold"} />
       </Button>
     </Flex>
   );
