aa85d830de63eb6adb48f98abde0d6f159a35cdc
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-22 18:39:44 +0000

    Adjustment in the AuthHeader component

--- src/authentication/components/AuthHeader.jsx
+++ src/authentication/components/AuthHeader.jsx
@@ -1,13 +1,26 @@
-import Paragraph from "/src/ui/Paragraph";
-import Logo from "/src/components/Logo";
 import Header from "/src/ui/Header";
+import Logo from "/src/components/Logo";
+import Paragraph from "/src/ui/Paragraph";
 
-export default function AuthHeader() {
+export default function AuthHeader({ type }) {
   return (
-    <Header classname={"flex flex-col items-center justify-center"}>
-      <Logo show={true} />
-      <Paragraph classname="medium:text-base middle:text-lg mt-1 text-sm font-bold text-slate-500">
-        WalkWise
+    <Header 
+      classname={"w-full flex flex-col items-center justify-start mb-6"}
+    >
+      <div 
+        className={"w-full flex flex-row gap-2 items-center justify-start mb-12"}
+      >
+        <Logo show={true} />
+        <Paragraph 
+          classname={"mt-1 text-3xl font-bold text-slate-500"}
+        >
+          WalkWise
+        </Paragraph>
+      </div>
+      <Paragraph 
+        classname={"text-start w-full mt-1 text-2xl font-bold"}
+      >
+        {type ? "Create account" : "Login account"}
       </Paragraph>
     </Header>
   );
