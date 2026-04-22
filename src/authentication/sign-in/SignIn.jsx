91e655762451b1d29fdc11120170f18fe3b026d6
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-22 18:40:15 +0000

    Refactoring of code in the Signin component

--- src/authentication/sign-in/SignIn.jsx
+++ src/authentication/sign-in/SignIn.jsx
@@ -1,27 +1,52 @@
-import AuthCloseButton from "../components/AuthCloseButton";
-import AuthHeader from "../components/AuthHeader";
-import SignInForm from "./components/SignInForm";
-import Container from "/src/ui/Container";
 import Flex from "/src/ui/Flex";
-import Box from "/src/ui/Box";
 import Button from "/src/ui/Button";
+import Container from "/src/ui/Container";
+import Paragraph from "/src/ui/Paragraph";
+import SignInForm from "./components/SignInForm";
+import AuthHeader from "../components/AuthHeader";
+import AuthCloseButton from "../components/AuthCloseButton";
 
 export default function SignIn() {
   return (
-    <Container>
+    <Container 
+      id="sign-in"
+      classname={"flex items-center justify-center flex-col"}
+    >
       <AuthCloseButton />
-      <Flex variant="center" classname={"min-h-[90vh]"}>
-        <Box
-          adjustWidth={true}
-          classname={"rounded-xl border border-stone-300 p-6 shadow-lg"}
+      <Flex 
+        variant="center" 
+        classname={"justify-center min-h-[90vh] w-full max-w-md flex-col px-8 medium:px-0"}
+      >
+        <AuthHeader />
+        <SignInForm />
+
+        <Button 
+          to="/forgotten" 
+          type="buttonText"
+          ariaLabel="Forget Password"
+          classname={"text-left w-full whitespace-nowrap text-slate-400 my-4 text-sm font-semibold"}
         >
-          <AuthHeader />
-          <SignInForm />
-          <Flex variant="between" classname={"mt-4"}>
-            <Button to="/forgotten">Forget Password?</Button>
-            <Button to="/sign-up">Sign Up</Button>
-          </Flex>
-        </Box>
+          Forget Password?
+        </Button>
+        
+        <Flex 
+          variant="center" 
+          classname={"flex-row gap-2"}
+        >
+          <Paragraph 
+            classname={"text-sm whitespace-nowrap text-slate-400"}
+          >
+            Don't have an account?
+          </Paragraph>
+          <Button 
+            to="/sign-up"
+            type="buttonText"
+            ariaLabel="Sign Up"
+            classname={"text-sm font-semibold"}
+          >
+            Sign Up
+          </Button>
+        </Flex>
       </Flex>
     </Container>
   );
