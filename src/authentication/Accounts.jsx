4205c03a83f55d93dbc760ff29732e89a14f90e2
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-29 20:27:10 +0000

    Adjustment in the Account component

--- src/authentication/Accounts.jsx
+++ src/authentication/Accounts.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Accounts() {
   return (
-    <div className={"h-screen w-full"}>
+    <div className={"h-screen w-full overflow-x-hidden"}>
       <Outlet />
     </div>
   );
