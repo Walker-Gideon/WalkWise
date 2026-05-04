88927b16a4deffea4f3d5837c58c303d480028bf
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-04 14:17:19 +0000

    Create and update the forget password hook

--- src/authentication/hooks/useForgetPssword.js
+++ src/authentication/hooks/useForgetPssword.js
@@ -1,13 +1,17 @@
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 
 import { sendResetPasswordLink } from "/src/service/apiAuth";
 
 export const useForgetPassword = () => {
+    const navigate = useNavigate();
+    
     const {mutate:forgetPassword, isPending, isSuccess } = useMutation({
       mutationFn: ({email}) => sendResetPasswordLink(email),
-      onSuccess: () => {
+      onSuccess: (data) => {
         toast.success("Password reset link sent successfully");
+        navigate("/verify-email", { state: { email: data.email } });
       },
       onError: (error) => {
         toast.error(error.message);
@@ -15,4 +19,4 @@ export const useForgetPassword = () => {
     });
 
     return {forgetPassword, isPending, isSuccess };
-}
+}
