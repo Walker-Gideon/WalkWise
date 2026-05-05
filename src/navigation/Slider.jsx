7cb5a7cfcda5ac4d2b01713f4e448c83b392de93
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-05 13:27:48 +0000

    Create the logout button and add it functionality

--- src/navigation/Slider.jsx
+++ src/navigation/Slider.jsx
@@ -1,10 +1,15 @@
+import { LuLogOut } from "react-icons/lu";
+
 import Box from "/src/ui/Box";
 import Aside from "/src/ui/Aside";
+import Button from "/src/ui/Button";
 import MainNav from "./components/MainNav";
 import UserProfile from "/src/user/UserProfile";
 import ThemeButton from "./components/ThemeButton";
 import NavigationHeader from "./components/NavigationHeader";
 
+import useNavigateToAction from "/src/hook/useNavigateToAction";
+import { useAuthentication } from "/src/authentication/context/AuthContext";
 import { useNav, NavigationContext } from "/src/contexts/NavigationContext";
 
 export default function Slider({ menu }) {
@@ -24,11 +29,40 @@ export default function Slider({ menu }) {
           <NavigationHeader />
           <MainNav />
         </Box>
-        <Box adjustWidth={true} classname={"borderStyling border-t p-4"}>
+        <Box
+          adjustWidth={true}
+          classname={"flex flex-col gap-4 borderStyling border-t p-3.5"}
+        >
           <ThemeButton />
+          <LogOutButton />
           <UserProfile />
         </Box>
       </NavigationContext.Provider>
     </Aside>
+  );
+}
+
+function LogOutButton() {
+  const { isExpanded } = useNav();
+  const { logout } = useAuthentication();
+  const navigateTo = useNavigateToAction();
+
+  const handleLogout = async () => {
+    await logout();
+    navigateTo("/sign-in");
+  };
+
+  return (
+    <Button
+      variant="secondary"
+      ariaLabel="Sign out"
+      onclick={handleLogout}
+      classname={
+        `flex items-center space-x-2 text-sm font-semibold text-slate-900 dark:text-slate-300 py-2 px-2 rounded-sm transition ${isExpanded ? "medium:justify-center medium:text-center" : ""}`
+      }
+    >
+      <LuLogOut className="text-base" />
+      <span className={`${isExpanded ? "medium:hidden" : "medium:block"}`}>Sign out</span>
+    </Button>
   );
 }
