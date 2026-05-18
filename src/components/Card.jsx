d08cb3818edf1334926229699a429ca3dd9e9bca
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-18 18:01:26 +0000

    Adjustment in the Card UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
-export default function Card({ children, classname, status }) {
+export default function Card({ children, classname, status, bgcolor="bg-white/70 dark:bg-slate-900/70" }) {
   return (
     <div
       // if hover hover:shadow-lg
-      className={`rounded-xl border border-stone-300 bg-white/70 backdrop-blur-xl transition-all duration-300 dark:border-slate-700 dark:bg-slate-900/70 ${classname} ${status ? "p-4.5 medium:p-6" : "p-6"}`}
+      className={`rounded-xl border border-stone-300 backdrop-blur-xl transition-all duration-300 dark:border-slate-700 ${bgcolor} ${classname} ${status ? "p-4.5 medium:p-6" : "p-6"}`}
     >
       {children}
     </div>
