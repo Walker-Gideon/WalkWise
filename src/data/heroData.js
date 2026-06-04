565e787505587f3c1bcfbff9831be23b0a9f826b
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-04 13:59:31 +0000

    feat: add heroData text for the image alt tag

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -2,19 +2,20 @@ import hero1 from "/src/assets/hero1.png";
 import hero2 from "/src/assets/hero2.png";
 import hero3 from "/src/assets/hero3.png";
 
-const heroData = [
+export const heroData = [
   {
     url: hero1,
     style: "-rotate-12",
+    text: "WalkWise notes",
   },
   {
     url: hero2,
     style: "mt-4 md:mt-6",
+    text: "WalkWise flashcards",
   },
   {
     url: hero3,
     style: "rotate-12",
+    text: "WalkWise study schedule planner",
   },
 ];
-
-export default heroData;
