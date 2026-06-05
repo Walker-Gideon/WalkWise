90bc20e905455564ac2d135958614f37fe742fc3
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-05 23:54:03 +0000

    feat: configure manual rollup chunk splitting

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,27 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("firebase")) {
+              return "vendor-firebase";
+            }
+            if (id.includes("@tanstack")) {
+              return "vendor-query";
+            }
+            if (id.includes("date-fns")) {
+              return "vendor-date";
+            }
+            if (id.includes("react-icons") || id.includes("lucide")) {
+              return "vendor-icons";
+            }
+            return "vendor-misc";
+          }
+        },
+      },
+    },
+  },
 })
