da2ec12e0043c53618d4e30fa70edc56f097c97d
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-13 21:32:05 +0000

    fix: refactor imports and improve code formatting in AppLayout component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,11 @@
+import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "motion/react";
+import { AnimatePresence, motion as Motion } from "motion/react";
 
-import Slider from "/src/navigation/Slider";
-import Container from "/src/ui/Container";
-import Model from "/src/components/Model"
 import Main from "/src/ui/Main";
+import Model from "/src/components/Model";
+import Container from "/src/ui/Container";
+import Slider from "/src/navigation/Slider";
 
 import { useNav } from "/src/contexts/NavigationContext";
 
@@ -12,17 +13,19 @@ export default function AppLayout() {
   const { isMobileMenuOpen, closeMobileMenu } = useNav();
   const location = useLocation();
 
+  useEffect(() => {
+    closeMobileMenu();
+  }, [location.pathname]);
+
   return (
-    <Container
-      classname={`defaultColor flex overflow-hidden`}
-    >
+    <Container classname={`defaultColor flex overflow-hidden`}>
       <Slider menu={false} />
       <AnimatePresence>
         {isMobileMenuOpen && (
-          <Model 
-            menu={true} 
+          <Model
+            menu={true}
             styling={"md:hidden"}
-            onClick={closeMobileMenu} 
+            onClick={closeMobileMenu}
             key="mobile-menu"
           >
             <Slider menu={true} />
@@ -31,19 +34,20 @@ export default function AppLayout() {
       </AnimatePresence>
 
       <Main
-        classname={"h-full w-full overflow-y-auto bg-slate-50 dark:bg-slate-800 transition"}
+        classname={
+          "h-full w-full overflow-y-auto bg-slate-50 dark:bg-slate-800 transition"
+        }
       >
-        <AnimatePresence mode="wait">
-          <motion.section
+        <AnimatePresence>
+          <Motion.section
             key={location.pathname}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.18, ease: "easeOut" }}
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.15, ease: "easeOut" }}
             className="h-full"
           >
             <Outlet />
-          </motion.section>
+          </Motion.section>
         </AnimatePresence>
       </Main>
     </Container>
