2094be3f63d0e664027f74b695fecf465aec3c0f
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-13 11:29:52 +0000

    fix: changes in the styling of the MainNav component

--- src/navigation/components/MainNav.jsx
+++ src/navigation/components/MainNav.jsx
@@ -64,7 +64,7 @@ export default function MainNav({ showLabel }) {
             }
           >
             <List classname={"flex items-center gap-2.5"}>
-              <SpanText className="shrink-0">
+              <SpanText classname="shrink-0">
                 <data.icon className="text-sm" />
               </SpanText>
               <SpanText
