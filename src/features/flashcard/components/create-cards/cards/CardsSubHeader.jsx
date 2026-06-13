87428d5950c7572467643f1b1aacb5d834e46b14
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-13 23:56:54 +0000

    fix: update CardsSubHeader to include flashcard availability check in Input component

--- src/features/flashcard/components/create-cards/cards/CardsSubHeader.jsx
+++ src/features/flashcard/components/create-cards/cards/CardsSubHeader.jsx
@@ -6,35 +6,39 @@ import Input from "/src/ui/Input";
 import Filter from "/src/components/Filter";
 import InitCardActionButton from "./InitCardActionButton";
 
+import { useFetchCards } from "/src/hook/useCards";
 import { useFlashcard } from "../../../context/FlashcardContext";
 
 export default function CardsSubHeader() {
-  const {
-    query,
-    setQuery
-  } = useFlashcard();
+  const { flashcards } = useFetchCards();
+  const { query, setQuery } = useFlashcard();
 
   return (
-    <Group classname={"w-full sticky top-19 md:top-20 z-30 py-6 px-6 bg-slate-50 dark:bg-slate-800 transition-colors duration-300 border-t borderStyling"}>
-    <Flex variant="between" classname={"gap-5 md:gap-1 flex-col md:flex-row"}>
-      <Group classname={"relative w-full md:w-1/2"}>
-        <Input
-          type="text"
-          name="query"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search flashcards"
-          classname={"w-full bg-transparent dark:text-white"}
-        />
-        <LuSearch className="absolute top-2.5 right-2 text-sm text-slate-600 dark:text-slate-300" />
-      </Group>
+    <Group
+      classname={
+        "w-full sticky top-19 md:top-20 z-30 py-6 px-6 bg-slate-50 dark:bg-slate-800 transition-colors duration-300 border-t borderStyling"
+      }
+    >
+      <Flex variant="between" classname={"gap-5 md:gap-1 flex-col md:flex-row"}>
+        <Group classname={"relative w-full md:w-1/2"}>
+          <Input
+            type="text"
+            name="query"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            disabled={!flashcards || flashcards.length === 0}
+            placeholder="Search flashcards"
+            classname={"w-full bg-transparent dark:text-white"}
+          />
+          <LuSearch className="absolute top-2.5 right-2 text-sm text-slate-600 dark:text-slate-300" />
+        </Group>
 
-      <Flex variant="between" classname={"w-full md:w-auto"}>
-        <FilterDirection align="right" styling={false} />
-        <FilterDirection styling={true} />
-        <InitCardActionButton styling={true} />
+        <Flex variant="between" classname={"w-full md:w-auto"}>
+          <FilterDirection align="right" styling={false} />
+          <FilterDirection styling={true} />
+          <InitCardActionButton styling={true} />
+        </Flex>
       </Flex>
-    </Flex>
     </Group>
   );
 }
@@ -42,11 +46,14 @@ export default function CardsSubHeader() {
 function FilterDirection({ align = "left", styling }) {
   return (
     <Group classname={`${styling ? "md:hidden" : "hidden md:block"}`}>
-      <Filter align={align} options={[
-        { value: "title", label: "Flashcard name" },
-        { value: "count", label: "Number of cards" },
-        { value: "time", label: "Time created" },
-      ]} />
+      <Filter
+        align={align}
+        options={[
+          { value: "title", label: "Flashcard name" },
+          { value: "count", label: "Number of cards" },
+          { value: "time", label: "Time created" },
+        ]}
+      />
     </Group>
   );
 }
