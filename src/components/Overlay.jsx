33c8d55ad2f246920a3c5ce2ebc7c30ec5d3dda4
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-13 12:57:24 +0000

    fix: update Overlay component styling for improved z-index and positioning

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -2,7 +2,7 @@ import Group from "/src/ui/Group";
 
 export default function Overlay({ children, classname, button, onclick }) {
   const styling =
-    "absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm z-40";
+    "fixed inset-0 z-50 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm";
 
   if (button)
     return (
