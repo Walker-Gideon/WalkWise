e954af58e40be799d1fa11aaad8d2d188d334418
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-14 03:48:43 +0000

    feat: implemented the chat header

---
+++ src/features/chat/ChatHeader.jsx
@@ -0,0 +1,30 @@
+import { LuChevronsRight, LuRefreshCw } from "react-icons/lu";
+
+import Button from "/src/ui/Button";
+import HeaderText from "/src/ui/HeaderText";
+
+import { useChat } from "/src/contexts/useChat.js";
+
+export default function ChatHeader() {
+  const { closeChat, refreshChat, isRefreshing } = useChat();
+
+  return (
+    <header className="mb-4 flex h-10 w-full items-center justify-between">
+      <div className="flex items-center space-x-1">
+        <Button variant="text" onClick={closeChat}>
+          <LuChevronsRight className="h-6 w-6 text-slate-400 dark:text-slate-500" />
+        </Button>
+        <HeaderText type="secondary">Study Assistant</HeaderText>
+      </div>
+
+      <Button
+        disabled={isRefreshing}
+        variant="text"
+        classname="disabled:cursor-not-allowed"
+        onClick={refreshChat}
+      >
+        <LuRefreshCw className="h-5 w-5 text-slate-400 dark:text-slate-500" />
+      </Button>
+    </header>
+  );
+}
