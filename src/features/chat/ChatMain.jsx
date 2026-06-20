b71b6d1ec3580592bf3ea4262a2c248a33b5e883
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-20 15:38:14 +0000

    feat: add the ChatMain component to hold other component of the chat section

--- src/features/chat/ChatMain.jsx
+++ src/features/chat/ChatMain.jsx
@@ -1,16 +1,21 @@
+import Main from "/src/ui/Main";
+import Container from "/src/ui/Container";
 import MessageInput from "./MessageInput";
 import RenderMessage from "./RenderMessage";
 import TypingIndicator from "./TypingIndicator";
 
 export default function ChatMain({ messagesEndRef, handleSubmit }) {
   return (
-    <div className="relative flex h-screen flex-1 flex-col overflow-hidden p-1">
-      <div className="scroll-container flex-1 overflow-y-auto pr-2">
+    <Container
+      adjust={true}
+      classname={"relative flex h-full flex-1 flex-col overflow-hidden p-1"}
+    >
+      <Main classname={"scroll-container flex-1 overflow-y-auto pr-2"}>
         <RenderMessage />
         <TypingIndicator />
         <div ref={messagesEndRef} />
-      </div>
+      </Main>
       <MessageInput handleSubmit={handleSubmit} />
-    </div>
+    </Container>
   );
 }
